e104acbcbb19e376994088e29ffc31e23d51a377
Jason Cobb <jason.e.cobb@gmail.com>
2020-08-24 22:14:24 -0400

    Make tests actually work with gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,22 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test-junit5"))
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.3")
     implementation("net.dv8tion:JDA:4.2.0_198")
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit5"))
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.5.2")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "13"
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
