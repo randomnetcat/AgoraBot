a6dcf19e7ea498a63a76ec03c918cd5b64b6e511
Jason Cobb <jason.e.cobb@gmail.com>
2020-08-26 13:34:38 -0400

    Add Error type to CommandArgumentParser

--- src/main/kotlin/org/randomcat/agorabot/commands/Parsing.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/Parsing.kt
@@ -1,55 +1,60 @@
 package org.randomcat.agorabot.commands
 
-sealed class CommandArgumentParseResult<out T> {
-    object Failure : CommandArgumentParseResult<Nothing>()
-    data class FailureWithMessage(val message: String) : CommandArgumentParseResult<Nothing>()
-    data class Success<out T>(val value: T, val remaining: UnparsedCommandArgs) : CommandArgumentParseResult<T>()
+sealed class CommandArgumentParseResult<out T, out E> {
+    data class Failure<out E>(
+        val error: E,
+    ) : CommandArgumentParseResult<Nothing, E>()
+
+    data class Success<out T>(
+        val value: T,
+        val remaining: UnparsedCommandArgs,
+    ) : CommandArgumentParseResult<T, Nothing>()
 }
 
-interface CommandArgumentParser<out T> {
-    fun parse(arguments: UnparsedCommandArgs): CommandArgumentParseResult<T>
+interface CommandArgumentParser<out T, out E> {
+    fun parse(arguments: UnparsedCommandArgs): CommandArgumentParseResult<T, E>
 }
 
-private typealias CAP<T> = CommandArgumentParser<T>
+private typealias CAP<T, E> = CommandArgumentParser<T, E>
 
 interface ArgumentDescriptionReceiver {
-    fun argsRaw(vararg parsers: CommandArgumentParser<Any?>, exec: CommandExecutionReceiverBlock<List<Any?>>)
+    fun <T, E> argsRaw(vararg parsers: CommandArgumentParser<T, E>, exec: CommandExecutionReceiverBlock<List<T>>)
 }
 
 private typealias CDR = ArgumentDescriptionReceiver
 
-inline fun <A> CDR.args(
-    a: CAP<A>,
+inline fun <A, AE> CDR.args(
+    a: CAP<A, AE>,
     crossinline exec: CERB<CommandArgs1<A>>,
 ) = argsRaw(a) {
     val args = args
     execWithNewArgs(exec, CommandArgs1(args[0] as A))
 }
 
-inline fun <A, B> CDR.args(
-    a: CAP<A>,
-    b: CAP<B>,
+inline fun <A, AE, B, BE> CDR.args(
+    a: CAP<A, AE>,
+    b: CAP<B, BE>,
     crossinline exec: CERB<CommandArgs2<A, B>>,
 ) = argsRaw(a, b) {
     val args = args
     execWithNewArgs(exec, CommandArgs2(args[0] as A, args[1] as B))
 }
 
-inline fun <A, B, C> CDR.args(
-    a: CAP<A>,
-    b: CAP<B>,
-    c: CAP<C>,
+inline fun <A, AE, B, BE, C, CE> CDR.args(
+    a: CAP<A, AE>,
+    b: CAP<B, BE>,
+    c: CAP<C, CE>,
     crossinline exec: CERB<CommandArgs3<A, B, C>>,
 ) = argsRaw(a, b, c) {
     val args = args
     execWithNewArgs(exec, CommandArgs3(args[0] as A, args[1] as B, args[2] as C))
 }
 
-inline fun <A, B, C, D> CDR.args(
-    a: CAP<A>,
-    b: CAP<B>,
-    c: CAP<C>,
-    d: CAP<D>,
+inline fun <A, AE, B, BE, C, CE, D, DE> CDR.args(
+    a: CAP<A, AE>,
+    b: CAP<B, BE>,
+    c: CAP<C, CE>,
+    d: CAP<D, DE>,
     crossinline exec: CERB<CommandArgs4<A, B, C, D>>,
 ) = argsRaw(a, b, c, d) {
     val args = args
