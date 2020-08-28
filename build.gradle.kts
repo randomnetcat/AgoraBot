0a9df0747eb5f2cd406567f3f545ae9f41a474d4
Jason Cobb <jason.e.cobb@gmail.com>
2020-08-28 12:50:32 -0400

    Add kotlinx.serialization dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.0"
+    kotlin("plugin.serialization") version "1.4.0"
 }
 group = "org.randomcat"
 version = "1.0-SNAPSHOT"
@@ -14,6 +15,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.3")
     implementation("net.dv8tion:JDA:4.2.0_198")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC")
     runtimeOnly("org.slf4j:slf4j-simple:1.7.30")
 
     testImplementation(kotlin("test"))
