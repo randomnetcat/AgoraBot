ce627ea797bc046620a278d412ad297139573bdf
Jason Cobb <jason.e.cobb@gmail.com>
2020-08-31 11:06:21 -0400

    Add single-argument version of help

--- src/main/kotlin/org/randomcat/agorabot/commands/Help.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/Help.kt
@@ -1,28 +1,52 @@
 package org.randomcat.agorabot.commands
 
 import net.dv8tion.jda.api.MessageBuilder
+import org.randomcat.agorabot.Command
 import org.randomcat.agorabot.QueryableCommandRegistry
+
+private fun MessageBuilder.appendUsage(name: String, command: Command) {
+    val usageHelp =
+        if (command is BaseCommand)
+            command.usage().ifBlank { NO_ARGUMENTS }
+        else
+            "<no usage available>"
+
+    append(name, MessageBuilder.Formatting.BOLD)
+    append(": $usageHelp")
+}
 
 class HelpCommand(private val registry: QueryableCommandRegistry) : ChatCommand() {
     override fun TopLevelArgumentDescriptionReceiver<ExecutionReceiverImpl>.impl() {
-        noArgs {
-            val commands = registry.commands()
-            val builder = MessageBuilder()
+        matchFirst {
+            noArgs {
+                val commands = registry.commands()
+                val builder = MessageBuilder()
 
-            for ((name, command) in commands) {
-                val usageHelp =
-                    if (command is BaseCommand)
-                        command.usage().ifBlank { NO_ARGUMENTS }
-                    else
-                        "<no usage available>"
+                for ((name, command) in commands) {
+                    builder.appendUsage(name = name, command = command)
+                    builder.appendLine()
+                }
 
-                builder.append(name, MessageBuilder.Formatting.BOLD)
-                builder.append(": $usageHelp")
-                builder.appendLine()
+                if (!builder.isEmpty) {
+                    builder.buildAll(MessageBuilder.SplitPolicy.NEWLINE).forEach { respond(it) }
+                }
             }
 
-            if (!builder.isEmpty) {
-                builder.buildAll(MessageBuilder.SplitPolicy.NEWLINE).forEach { respond(it) }
+            args(StringArg("command")) { args ->
+                val commandName = args.first
+                val commands = registry.commands()
+
+                if (commands.containsKey(commandName)) {
+                    val command = commands.getValue(commandName)
+
+                    val builder = MessageBuilder()
+                    builder.appendUsage(name = commandName, command = command)
+                    builder.appendLine()
+
+                    respond(builder.build())
+                } else {
+                    respond("No such command \"$commandName\".")
+                }
             }
         }
     }
