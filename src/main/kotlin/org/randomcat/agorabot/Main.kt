a0fdf9948edc7f6bca235dc75f8ad612770339ae
Jason Cobb <jason.e.cobb@gmail.com>
2020-09-03 20:45:00 -0400

    Add more helpful send strategy parsing, also changing format slightly

--- src/main/kotlin/org/randomcat/agorabot/Main.kt
+++ src/main/kotlin/org/randomcat/agorabot/Main.kt
@@ -1,6 +1,7 @@
 package org.randomcat.agorabot
 
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import net.dv8tion.jda.api.JDABuilder
@@ -13,33 +14,68 @@ import org.slf4j.LoggerFactory
 import java.nio.file.Files
 import java.nio.file.Path
 
+private fun readDigestSendStrategy(mailConfig: JsonObject, digestFormat: DigestFormat): DigestSendStrategy? {
+    val digestObject = mailConfig["digest"]
+    if (digestObject == null) {
+        logger.warn(
+            "Mail config does not contain digest section! Digest sending will be disabled. " +
+                    "Set digest.strategy = \"none\" to silence this warning."
+        )
+        return null
+    }
+
+    if (digestObject !is JsonObject) {
+        logger.error("Mail config digest section should be a JSON object!")
+        return null
+    }
+
+    val sendStrategyName = digestObject["send_strategy"]?.jsonPrimitive?.content
+    if (sendStrategyName == null) {
+        logger.error("Digest object did not contain send_strategy!")
+        return null
+    }
+
+    when (sendStrategyName) {
+        "none" -> return null
+
+        "ssmtp" -> {
+            val ssmtpPath = digestObject["ssmtp_path"]?.jsonPrimitive?.content.let { Path.of(it) }
+            if (ssmtpPath == null) {
+                logger.error("ssmtp_path should be set!")
+                return null
+            }
+
+            val ssmtpConfigPath = digestObject["ssmtp_config_path"]?.jsonPrimitive?.content.let { Path.of(it) }
+            if (ssmtpConfigPath == null) {
+                logger.error("ssmtp_config_path should be set!")
+                return null
+            }
+
+            return SsmtpDigestSendStrategy(
+                digestFormat = digestFormat,
+                executablePath = ssmtpPath,
+                configPath = ssmtpConfigPath,
+            )
+        }
+
+        else -> {
+            logger.error("Unrecognized send_strategy \"$sendStrategyName\"!")
+            return null
+        }
+    }
+}
+
 private fun digestCommand(digestMap: GuildDigestMap): Command {
     val mailConfigPath = Path.of(".", "mail.json")
     require(Files.exists(mailConfigPath)) { "mail.json does not exist" }
 
     val mailConfig = Json.parseToJsonElement(Files.readString(mailConfigPath, Charsets.UTF_8)).jsonObject
 
-    val ssmtpPath = Path.of(
-        (mailConfig["ssmtp_path"] ?: error("must specify ssmtp path in mail config"))
-            .jsonPrimitive
-            .content
-    )
-
-    val ssmtpConfigPath = Path.of(
-        (mailConfig["ssmtp_config_path"] ?: error("must specify ssmtp config path in mail config"))
-            .jsonPrimitive
-            .content
-    )
-
     val digestFormat = DefaultDigestFormat()
 
     return DigestCommand(
         digestMap,
-        SsmtpDigestSendStrategy(
-            digestFormat = digestFormat,
-            executablePath = ssmtpPath,
-            configPath = ssmtpConfigPath,
-        ),
+        readDigestSendStrategy(mailConfig = mailConfig, digestFormat = digestFormat),
         digestFormat,
     )
 }
