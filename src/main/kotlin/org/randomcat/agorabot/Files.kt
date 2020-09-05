cafae72414e337fb649241c17e7018f87759d628
Jason Cobb <jason.e.cobb@gmail.com>
2020-09-05 16:40:38 -0400

    Add withTempFile

---
+++ src/main/kotlin/org/randomcat/agorabot/Files.kt
@@ -0,0 +1,19 @@
+package org.randomcat.agorabot
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+/**
+ * Creates a temp file, invokes [block] with it, then deletes it (even if [block] throws).
+ *
+ * @param comment a comment on the use of the file - its usage is unspecified
+ */
+inline fun <R> withTempFile(comment: String? = null, block: (Path) -> R): R {
+    val tempFile: Path = Files.createTempFile("agorabot", comment)
+
+    return try {
+        block(tempFile)
+    } finally {
+        Files.deleteIfExists(tempFile)
+    }
+}
---
+++ src/test/kotlin/org/randomcat/agorabot/WithTempFileTest.kt
@@ -0,0 +1,53 @@
+package org.randomcat.agorabot
+
+import org.junit.jupiter.api.assertThrows
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.test.*
+
+class WithTempFileTest {
+    @Test
+    fun `file is empty on creation`() {
+        withTempFile { file ->
+            assertTrue(Files.readAllBytes(file).isEmpty())
+        }
+    }
+
+    @Test
+    fun `returns block value`() {
+        val value = 12
+        assertEquals(value, withTempFile { value })
+    }
+
+    @Test
+    fun `file does not exist after block end`() {
+        val tempFilePath = withTempFile { it }
+        assertFalse(Files.exists(tempFilePath))
+    }
+
+    @Test
+    fun `propagates exception`() {
+        val exception = Exception()
+
+        assertSame(
+            exception,
+            assertThrows<Exception> { throw exception }
+        )
+    }
+
+    @Test
+    fun `file does not exist after block throw`() {
+        lateinit var tempFilePath: Path
+
+        try {
+            withTempFile {
+                tempFilePath = it
+                throw Exception()
+            }
+        } catch (e: Exception) {
+            /* ignored */
+        }
+
+        assertFalse(Files.exists(tempFilePath))
+    }
+}
