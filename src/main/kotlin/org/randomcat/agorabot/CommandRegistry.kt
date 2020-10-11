3d71d519c575a3069a932ed32f36a4ecc1808d6a
Jason Cobb <jason.e.cobb@gmail.com>
2020-10-11 17:00:58 -0400

    Remove unused MapCommandRegistry and NullCommandRegistry

--- src/main/kotlin/org/randomcat/agorabot/CommandRegistry.kt
+++ src/main/kotlin/org/randomcat/agorabot/CommandRegistry.kt
@@ -17,12 +17,6 @@ interface QueryableCommandRegistry : CommandRegistry {
     fun commands(): ImmutableMap<String, Command>
 }
 
-class NullCommandRegistry : CommandRegistry {
-    override fun invokeCommand(event: MessageReceivedEvent, invocation: CommandInvocation) {
-        /* do nothing */
-    }
-}
-
 interface Command {
     fun invoke(event: MessageReceivedEvent, invocation: CommandInvocation)
 }
@@ -31,27 +25,6 @@ typealias UnknownCommandHook = (MessageReceivedEvent, CommandInvocation) -> Unit
 
 private fun defaultUnknownCommand(event: MessageReceivedEvent, commandInvocation: CommandInvocation) {
     event.channel.sendMessage("Unknown command \"${commandInvocation.command}\".").disallowMentions().queue()
-}
-
-data class MapCommandRegistry(
-    private val registry: ImmutableMap<String, Command>,
-    private val unknownCommandHook: UnknownCommandHook,
-) : QueryableCommandRegistry {
-    constructor(
-        registry: Map<String, Command>,
-        unknownCommandHook: UnknownCommandHook = ::defaultUnknownCommand,
-    ) : this(
-        registry.toImmutableMap(),
-        unknownCommandHook,
-    )
-
-    override fun commands(): ImmutableMap<String, Command> {
-        return registry
-    }
-
-    override fun invokeCommand(event: MessageReceivedEvent, invocation: CommandInvocation) {
-        registry[invocation.command]?.invoke(event, invocation) ?: unknownCommandHook(event, invocation)
-    }
 }
 
 class MutableMapCommandRegistry(
