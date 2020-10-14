e95ff9756f2f6a1e28076aafd88de5d00e3cb926
Jason Cobb <jason.e.cobb@gmail.com>
2020-10-14 15:16:28 -0400

    Make normal CFJ responses more probably

--- src/main/kotlin/org/randomcat/agorabot/commands/CrystalBall.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/CrystalBall.kt
@@ -1,22 +1,24 @@
 package org.randomcat.agorabot.commands
 
-private val RESPONSES = listOf(
-    "TRUE",
-    "FALSE",
-    "DISMISS",
-    "IRRELEVANT",
-    "SHENANIGANS",
-    "I GUESS",
-    "PROBABLY",
-    "WHO KNOWS",
-    "OH GOD, DEFINITELY NOT",
-    "DON'T ASK ME",
-    "THE ONLY REAL ARGUMENT IN FAVOR OF THIS IS WISHFUL THINKING",
-    "A TYPICAL EXAMPLE OF \"I SAY I DO, THEREFORE I DO\" WHICH HAS PLAGUED AGORA FOR A LONG TIME",
-    "IF YOU SAY SO",
-    "EDIBLE",
-    "INEDIBLE",
-)
+import org.randomcat.agorabot.util.repeat
+
+private val RESPONSES =
+    listOf(
+        "TRUE",
+        "FALSE",
+        "DISMISS",
+        "IRRELEVANT",
+        "SHENANIGANS",
+    ).repeat(8) + listOf(
+        "I GUESS",
+        "PROBABLY",
+        "WHO KNOWS",
+        "OH GOD, DEFINITELY NOT",
+        "DON'T ASK ME",
+        "THE ONLY REAL ARGUMENT IN FAVOR OF THIS IS WISHFUL THINKING",
+        "A TYPICAL EXAMPLE OF \"I SAY I DO, THEREFORE I DO\" WHICH HAS PLAGUED AGORA FOR A LONG TIME",
+        "IF YOU SAY SO",
+    )
 
 class CrystalBallCommand(strategy: BaseCommandStrategy) : BaseCommand(strategy) {
     override fun TopLevelArgumentDescriptionReceiver<ExecutionReceiverImpl>.impl() {
