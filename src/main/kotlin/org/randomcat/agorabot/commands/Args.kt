aeb9fcb68e45c59162a7c428291979ed92a89f7f
Jason Cobb <jason.e.cobb@gmail.com>
2020-10-14 18:26:00 -0400

    Add RemainingStringArgs helper

--- src/main/kotlin/org/randomcat/agorabot/commands/Args.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/Args.kt
@@ -1,5 +1,8 @@
 package org.randomcat.agorabot.commands
 
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+
 interface NamedCommandArgument {
     val name: String
 }
@@ -9,7 +12,7 @@ interface TypedCommandArgument {
 }
 
 abstract class BaseCommandArgument<T>(
-    override val name: String
+    override val name: String,
 ) : CommandArgumentParser<T, ReadableCommandArgumentParseError>, NamedCommandArgument, TypedCommandArgument {
     protected val noArgumentError =
         CommandArgumentParseResult.Failure(ReadableCommandArgumentParseError("no argument provided"))
@@ -52,5 +55,17 @@ fun StringArg(name: String) = object : BaseCommandArgument<String>(name) {
         if (args.isEmpty()) return noArgumentError
 
         return CommandArgumentParseSuccess(args.first(), arguments.tail())
+    }
+}
+
+fun RemainingStringArgs(name: String) = object : BaseCommandArgument<List<String>>(name) {
+    override val type: String
+        get() = "string"
+
+    override val count: CommandArgumentUsage.Count
+        get() = CommandArgumentUsage.Count.REPEATING
+
+    override fun parse(arguments: UnparsedCommandArgs): CommandArgumentParseResult<ImmutableList<String>, ReadableCommandArgumentParseError> {
+        return CommandArgumentParseSuccess(arguments.args.toImmutableList(), UnparsedCommandArgs(emptyList()))
     }
 }
