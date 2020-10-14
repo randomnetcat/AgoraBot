f690a4b73c4910998f26d97e8b8d507146c74f89
Jason Cobb <jason.e.cobb@gmail.com>
2020-10-14 15:16:15 -0400

    Add Iterable.repeat helper

---
+++ src/main/kotlin/org/randomcat/agorabot/util/Collections.kt
@@ -0,0 +1,12 @@
+package org.randomcat.agorabot.util
+
+fun <T, U : T> Iterable<U>.repeat(times: Int): List<T> {
+    val source = this // For clarity
+    val effectiveSize = if (this is Collection) this.size else 10
+
+    return ArrayList<T>(initialCapacity = effectiveSize * times).also { res ->
+        repeat(times) {
+            res.addAll(source)
+        }
+    }
+}
