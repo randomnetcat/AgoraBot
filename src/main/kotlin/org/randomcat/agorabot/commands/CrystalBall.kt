b1796e8f7f049f063e3daea64e36bf782e01b6e5
Jason Cobb <jason.e.cobb@gmail.com>
2020-10-14 15:40:27 -0400

    Update CFJ responses again

--- src/main/kotlin/org/randomcat/agorabot/commands/CrystalBall.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/CrystalBall.kt
@@ -2,23 +2,28 @@ package org.randomcat.agorabot.commands
 
 import org.randomcat.agorabot.util.repeat
 
-private val RESPONSES =
-    listOf(
-        "TRUE",
-        "FALSE",
-        "DISMISS",
-        "IRRELEVANT",
-        "SHENANIGANS",
-    ).repeat(8) + listOf(
-        "I GUESS",
-        "PROBABLY",
-        "WHO KNOWS",
-        "OH GOD, DEFINITELY NOT",
-        "DON'T ASK ME",
-        "THE ONLY REAL ARGUMENT IN FAVOR OF THIS IS WISHFUL THINKING",
-        "A TYPICAL EXAMPLE OF \"I SAY I DO, THEREFORE I DO\" WHICH HAS PLAGUED AGORA FOR A LONG TIME",
-        "IF YOU SAY SO",
-    )
+private val UNLIKELY_RESPONSES = listOf(
+    "SHENANIGANS",
+    "I GUESS",
+    "PROBABLY",
+    "WHO KNOWS",
+    "OH GOD, DEFINITELY NOT",
+    "DON'T ASK ME",
+    "THE ONLY REAL ARGUMENT IN FAVOR OF THIS IS WISHFUL THINKING",
+    "A TYPICAL EXAMPLE OF \"I SAY I DO, THEREFORE I DO\" WHICH HAS PLAGUED AGORA FOR A LONG TIME",
+    "IF YOU SAY SO",
+    "UNFORTUNATELY",
+    "IT'S IN THE BEST INTEREST OF THE GAME FOR THIS TO BE TRUE",
+)
+
+private val LIKELY_RESPONSES = listOf(
+    "TRUE",
+    "FALSE",
+    "DISMISS",
+    "IRRELEVANT",
+)
+
+private val RESPONSES = LIKELY_RESPONSES.repeat(10) + UNLIKELY_RESPONSES
 
 class CrystalBallCommand(strategy: BaseCommandStrategy) : BaseCommand(strategy) {
     override fun TopLevelArgumentDescriptionReceiver<ExecutionReceiverImpl>.impl() {
