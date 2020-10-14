6734f4bdcf2a44a293875ce3906dc07e12d13782
Jason Cobb <jason.e.cobb@gmail.com>
2020-10-14 18:25:04 -0400

    Fix compiler error in repeat

--- src/main/kotlin/org/randomcat/agorabot/util/Collections.kt
+++ src/main/kotlin/org/randomcat/agorabot/util/Collections.kt
@@ -4,7 +4,7 @@ fun <T, U : T> Iterable<U>.repeat(times: Int): List<T> {
     val source = this // For clarity
     val effectiveSize = if (this is Collection) this.size else 10
 
-    return ArrayList<T>(initialCapacity = effectiveSize * times).also { res ->
+    return ArrayList<T>(/* capacity = */ effectiveSize * times).also { res ->
         repeat(times) {
             res.addAll(source)
         }
