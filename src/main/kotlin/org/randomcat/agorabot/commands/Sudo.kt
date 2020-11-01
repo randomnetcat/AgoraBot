209e72325e52c69431bd63a87fc0e7074cc2773c
Jason Cobb <jason.e.cobb@gmail.com>
2020-11-01 17:40:32 -0500

    Tweak SudoCommand behavior

--- src/main/kotlin/org/randomcat/agorabot/commands/Sudo.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/Sudo.kt
@@ -3,15 +3,21 @@ package org.randomcat.agorabot.commands
 import org.randomcat.agorabot.commands.impl.*
 import org.randomcat.agorabot.permissions.BotScope
 
+private val PERMISSION = BotScope.command("sudo")
+
 class SudoCommand(strategy: BaseCommandStrategy) : BaseCommand(strategy) {
     override fun BaseCommandImplReceiver.impl() {
         matchFirst {
-            noArgs().permissions(BotScope.command("sudo")) { _ ->
+            noArgs().permissions(PERMISSION) { _ ->
                 respond("You are now root.")
             }
 
             args(RemainingStringArgs("command")) {
-                respond("This incident has been reported.")
+                if (senderHasPermission(PERMISSION)) {
+                    respond("Done.")
+                } else {
+                    respond("This incident has been reported.")
+                }
             }
         }
     }
