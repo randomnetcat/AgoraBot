7281a897682a52c1dee2e1d909c3279085dfd986
Jason Cobb <jason.e.cobb@gmail.com>
2021-01-13 20:16:22 -0500

    Add VersioningStorage

---
+++ src/main/kotlin/org/randomcat/agorabot/config/Versioning.kt
@@ -0,0 +1,67 @@
+package org.randomcat.agorabot.config
+
+import kotlinx.collections.immutable.PersistentMap
+import kotlinx.collections.immutable.toPersistentMap
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.randomcat.agorabot.util.withTempFile
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import java.nio.file.StandardOpenOption
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+interface VersioningStorage {
+    fun versionFor(component: String): String?
+    fun setVersion(component: String, version: String)
+}
+
+class JsonVersioningStorage(private val storagePath: Path) : VersioningStorage {
+    companion object {
+        private val FILE_CHARSET = Charsets.UTF_8
+
+        fun readFromFile(path: Path): Map<String, String> {
+            if (Files.notExists(path)) return emptyMap()
+
+            val content = Files.readString(path, FILE_CHARSET)
+            return Json.decodeFromString<Map<String, String>>(content)
+        }
+
+        fun writeToFile(path: Path, data: Map<String, String>) {
+            withTempFile { tempFile ->
+                val content = Json.encodeToString<Map<String, String>>(data)
+
+                Files.writeString(
+                    tempFile,
+                    content,
+                    FILE_CHARSET,
+                    StandardOpenOption.TRUNCATE_EXISTING,
+                    StandardOpenOption.CREATE,
+                )
+
+                Files.move(tempFile, path, StandardCopyOption.REPLACE_EXISTING)
+            }
+        }
+    }
+
+    private val lock: Lock = ReentrantLock()
+    private var cache: PersistentMap<String, String> = readFromFile(storagePath).toPersistentMap()
+
+    override fun versionFor(component: String): String? {
+        return lock.withLock { cache[component] }
+    }
+
+    override fun setVersion(component: String, version: String) {
+        lock.withLock {
+            if (versionFor(component) != version) {
+                val updatedValue = cache.put(component, version)
+
+                writeToFile(storagePath, updatedValue)
+                cache = updatedValue
+            }
+        }
+    }
+}
