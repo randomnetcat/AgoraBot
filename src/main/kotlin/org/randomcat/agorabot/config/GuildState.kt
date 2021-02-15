64542d79acc9d728e1795bccdb2d6c220db2f5bc
Jason Cobb <jason.e.cobb@gmail.com>
2021-02-14 19:42:22 -0500

    Add support for updating multiple GuildState entries at once

--- src/main/kotlin/org/randomcat/agorabot/config/GuildState.kt
+++ src/main/kotlin/org/randomcat/agorabot/config/GuildState.kt
@@ -2,6 +2,7 @@ package org.randomcat.agorabot.config
 
 import kotlinx.collections.immutable.PersistentMap
 import kotlinx.collections.immutable.persistentMapOf
+import kotlinx.collections.immutable.putAll
 import kotlinx.collections.immutable.toPersistentMap
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
@@ -15,23 +16,50 @@ import java.nio.file.StandardOpenOption
 import java.util.concurrent.atomic.AtomicReference
 
 interface GuildState {
-    fun getString(key: String): String?
-    fun setString(key: String, value: String)
+    fun getStrings(keys: List<String>): List<String?>
+    fun getString(key: String): String? = getStrings(listOf(key)).single()
 
-    fun updateString(key: String, mapper: (old: String?) -> String)
+    fun setStrings(keys: List<String>, values: List<String>)
+    fun setString(key: String, value: String) = setStrings(listOf(key), listOf(value))
+
+    fun updateStrings(keys: List<String>, mapper: (old: List<String?>) -> List<String>)
+
+    fun updateString(key: String, mapper: (old: String?) -> String) = updateStrings(listOf(key)) { values ->
+        listOf(mapper(values.single()))
+    }
 }
 
 inline fun <reified T> GuildState.get(key: String): T? {
     return getString(key)?.let { Json.decodeFromString<T>(it) }
 }
 
+inline fun <reified T> GuildState.getMany(keys: List<String>): List<T?> {
+    return getStrings(keys).map { str -> str?.let { Json.decodeFromString<T>(it) } }
+}
+
 inline fun <reified T> GuildState.set(key: String, value: T) {
     setString(key, Json.encodeToString<T>(value))
+}
+
+inline fun <reified T> GuildState.setMany(keys: List<String>, values: List<T>) {
+    setStrings(keys, values.map { Json.encodeToString<T>(it) })
 }
 
 inline fun <reified T> GuildState.update(key: String, crossinline mapper: (old: T?) -> T) {
     updateString(key) { oldString ->
         Json.encodeToString<T>(mapper(oldString?.let { Json.decodeFromString<T>(it) }))
+    }
+}
+
+inline fun <reified T> GuildState.updateMany(keys: List<String>, crossinline mapper: (old: List<T?>) -> List<T>) {
+    updateStrings(keys) { oldStrings ->
+        mapper(
+            oldStrings.map { oldString ->
+                oldString?.let { Json.decodeFromString<T>(it) }
+            }
+        ).map {
+            Json.encodeToString<T>(it)
+        }
     }
 }
 
@@ -67,13 +95,38 @@ class JsonGuildState(private val storagePath: Path) : GuildState {
 
     private val data: AtomicReference<PersistentMap<String, String>> = AtomicReference(readFromFile(storagePath))
 
+    override fun getStrings(keys: List<String>): List<String?> {
+        val currentData = data.get()
+        return keys.map { currentData[it] }
+    }
+
     override fun getString(key: String): String? {
         return data.get()[key]
+    }
+
+    override fun setStrings(keys: List<String>, values: List<String>) {
+        require(keys.size == values.size)
+        val pairs = keys.zip(values)
+
+        data.updateAndGet {
+            it.putAll(pairs)
+        }
     }
 
     override fun setString(key: String, value: String) {
         data.updateAndGet {
             it.put(key, value)
+        }
+    }
+
+    override fun updateStrings(keys: List<String>, mapper: (old: List<String?>) -> List<String>) {
+        data.updateAndGet { currentData ->
+            keys
+                .map { currentData[it] }
+                .let(mapper)
+                .also { require(it.size == keys.size) }
+                .let { keys.zip(it) }
+                .let { currentData.putAll(it) }
         }
     }
 
