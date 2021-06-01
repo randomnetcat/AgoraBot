08f18e20dd6bd470229e0d6c72b95af7f7065d4a
Jason Cobb <jason.e.cobb@gmail.com>
2021-05-31 22:20:57 -0400

    Add four factors analysis to cfj command

--- src/main/kotlin/org/randomcat/agorabot/commands/Cfj.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/Cfj.kt
@@ -2,6 +2,9 @@ package org.randomcat.agorabot.commands
 
 import org.randomcat.agorabot.commands.impl.*
 import org.randomcat.agorabot.util.repeated
+import kotlin.random.Random
+
+private const val FOUR_FACTORS_LOTS = 25
 
 private val UNLIKELY_RESPONSES = listOf(
     "SHENANIGANS",
@@ -30,16 +33,50 @@ private val LIKELY_RESPONSES = listOf(
 
 private val RESPONSES = LIKELY_RESPONSES.repeated(10) + MODERATE_RESOPONSES.repeated(2) + UNLIKELY_RESPONSES
 
+private fun randomShouldFourFactor(): Boolean {
+    return Random.nextInt(FOUR_FACTORS_LOTS) == 0
+}
+
+private fun randomResponse(): String {
+    return RESPONSES.random()
+}
+
+private fun BaseCommandExecutionReceiver.respondFourFactor() {
+    fun textForFactor(factor: String): String {
+        val isTrue = Random.nextBoolean()
+        return "Is the statement supported by $factor? ${if (isTrue) "TRUE" else "FALSE"}"
+    }
+
+    respond("I will now begin a full four factors analysis.")
+
+    val fourFactorsResponse = buildString {
+        appendLine(textForFactor("game custom"))
+        appendLine(textForFactor("common sense"))
+        appendLine(textForFactor("past judgements"))
+        appendLine(textForFactor("consideration of the best interests of the game"))
+    }
+
+    respond(fourFactorsResponse)
+}
+
 class CfjCommand(strategy: BaseCommandStrategy) : BaseCommand(strategy) {
     override fun BaseCommandImplReceiver.impl() {
         matchFirst {
             noArgs { _ ->
-                respond("Judged ${RESPONSES.random()}.")
+                if (randomShouldFourFactor()) {
+                    respondFourFactor()
+                }
+
+                respond("Judged ${randomResponse()}.")
             }
 
             args(RemainingStringArgs("statement")) { (statementParts) ->
+                if (randomShouldFourFactor()) {
+                    respondFourFactor()
+                }
+
                 val statement = statementParts.joinToString(" ")
-                respond("\"$statement\" judged ${RESPONSES.random()}.")
+                respond("\"$statement\" judged ${randomResponse()}.")
             }
         }
     }
