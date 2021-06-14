e677a6598faffdd34bae33331a416e918bd78da0
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-14 17:16:24 -0400

    Have SecretHitlerCommand take a repository parameter

--- src/main/kotlin/org/randomcat/agorabot/commands/SecretHitler.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/SecretHitler.kt
@@ -3,8 +3,12 @@ package org.randomcat.agorabot.commands
 import org.randomcat.agorabot.commands.impl.BaseCommand
 import org.randomcat.agorabot.commands.impl.BaseCommandImplReceiver
 import org.randomcat.agorabot.commands.impl.BaseCommandStrategy
+import org.randomcat.agorabot.secrethitler.SecretHitlerRepository
 
-class SecretHitlerCommand(strategy: BaseCommandStrategy) : BaseCommand(strategy) {
+class SecretHitlerCommand(
+    strategy: BaseCommandStrategy,
+    private val repository: SecretHitlerRepository,
+) : BaseCommand(strategy) {
     override fun BaseCommandImplReceiver.impl() {
         subcommands {
         }
--- src/main/kotlin/org/randomcat/agorabot/setup/Features.kt
+++ src/main/kotlin/org/randomcat/agorabot/setup/Features.kt
@@ -5,6 +5,9 @@ import org.randomcat.agorabot.commands.DiscordArchiver
 import org.randomcat.agorabot.config.parsing.features.CitationsConfig
 import org.randomcat.agorabot.config.parsing.features.readCitationsConfig
 import org.randomcat.agorabot.features.archiveCommandsFeature
+import org.randomcat.agorabot.secrethitler.JsonSecretHitlerChannelGameMap
+import org.randomcat.agorabot.secrethitler.JsonSecretHitlerGameList
+import org.randomcat.agorabot.secrethitler.SecretHitlerRepository
 import org.randomcat.agorabot.util.DefaultDiscordArchiver
 import java.nio.file.Path
 
@@ -30,5 +33,14 @@ fun setupArchiveFeature(paths: BotDataPaths): Feature {
     return archiveCommandsFeature(
         discordArchiver = setupArchiver(paths),
         localStorageDir = paths.archiveStorageDir(),
+    )
+}
+
+fun setupSecretHitlerFeature(paths: BotDataPaths): SecretHitlerRepository {
+    val secretHitlerDir = paths.storagePath.resolve("secret_hitler")
+
+    return SecretHitlerRepository(
+        gameList = JsonSecretHitlerGameList(storagePath = secretHitlerDir.resolve("games")),
+        channelGameMap = JsonSecretHitlerChannelGameMap(storagePath = secretHitlerDir.resolve("games_by_channel")),
     )
 }
