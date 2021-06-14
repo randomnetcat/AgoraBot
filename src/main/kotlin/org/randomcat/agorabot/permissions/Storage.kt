b1c933ffa8fa8e03c8ddd5779ab672243a266b1a
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-13 20:08:44 -0400

    Use AtomicCachedStorage for permissions

--- src/main/kotlin/org/randomcat/agorabot/permissions/Storage.kt
+++ src/main/kotlin/org/randomcat/agorabot/permissions/Storage.kt
@@ -7,30 +7,24 @@ import kotlinx.collections.immutable.toPersistentMap
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import org.randomcat.agorabot.config.AtomicCachedStorage
 import org.randomcat.agorabot.config.ConfigPersistService
+import org.randomcat.agorabot.config.StorageStrategy
 import org.randomcat.agorabot.util.AtomicLoadOnceMap
-import org.randomcat.agorabot.util.withTempFile
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.StandardCopyOption
-import java.nio.file.StandardOpenOption
-import java.util.concurrent.atomic.AtomicReference
 
-class JsonPermissionMap(private val storagePath: Path) : MutablePermissionMap {
-    companion object {
-        private val FILE_CHARSET = Charsets.UTF_8
+private typealias JsonValueType = PersistentMap<PermissionPath, PersistentMap<String, BotPermissionState>>
+private typealias JsonStorageType = Map<String, Map<String, String>>
 
-        private fun readFromFile(path: Path): PersistentMap<PermissionPath, PersistentMap<String, BotPermissionState>> {
-            if (Files.notExists(path)) return persistentMapOf()
+class JsonPermissionMap(storagePath: Path) : MutablePermissionMap {
+    private object Strategy : StorageStrategy<JsonValueType> {
+        override fun defaultValue(): JsonValueType {
+            return persistentMapOf()
+        }
 
-            // Convert to simpler type for storage
-            // Format: map of (permission path joined by PERMISSION_PATH_SEPARATOR) to (
-            //     map of (entity id) to (enum name of BotPermissionState)
-            // )
-            val stored = Json.decodeFromString<Map<String, Map<String, String>>>(Files.readString(path, FILE_CHARSET))
-
-            return stored
-                .asIterable()
+        private fun JsonStorageType.toValue(): JsonValueType {
+            return asIterable()
                 .associate { (permissionPath, idMap) ->
                     PermissionPath.fromSplitting(permissionPath) to
                             idMap.mapValues { (_, state) -> BotPermissionState.valueOf(state) }.toPersistentMap()
@@ -38,41 +32,36 @@ class JsonPermissionMap(private val storagePath: Path) : MutablePermissionMap {
                 .toPersistentMap()
         }
 
-        private fun writeToFile(path: Path, value: Map<PermissionPath, Map<String, BotPermissionState>>) {
-            val converted = value.asIterable().associate { (permissionPath, idMap) ->
-                permissionPath.joinToString() to
-                        idMap.mapValues { (_, state) -> state.name }
+        private fun JsonValueType.toStorageValue(): JsonStorageType {
+            return asIterable().associate { (path, idMap) ->
+                path.joinToString() to idMap.mapValues { (_, state) -> state.name }
             }
+        }
 
-            val content = Json.encodeToString<Map<String, Map<String, String>>>(converted)
+        override fun encodeToString(value: JsonValueType): String {
+            return Json.encodeToString<JsonStorageType>(value.toStorageValue())
+        }
 
-            withTempFile { tempFile ->
-                Files.writeString(
-                    tempFile,
-                    content,
-                    FILE_CHARSET,
-                    StandardOpenOption.TRUNCATE_EXISTING,
-                    StandardOpenOption.CREATE,
-                )
-
-                Files.move(tempFile, path, StandardCopyOption.REPLACE_EXISTING)
-            }
+        override fun decodeFromString(text: String): JsonValueType {
+            return Json.decodeFromString<JsonStorageType>(text).toValue()
         }
     }
 
-    private val map: AtomicReference<PersistentMap<PermissionPath, PersistentMap<String, BotPermissionState>>> =
-        AtomicReference(readFromFile(storagePath))
+    private val impl = AtomicCachedStorage<JsonValueType>(
+        storagePath = storagePath,
+        strategy = Strategy,
+    )
 
     fun schedulePersistenceOn(persistenceService: ConfigPersistService) {
-        persistenceService.schedulePersistence({ map.get() }, { writeToFile(storagePath, it) })
+        impl.schedulePersistenceOn(persistenceService)
     }
 
     override fun stateForId(path: PermissionPath, id: PermissionMapId): BotPermissionState? {
-        return map.get().get(path)?.get(id.raw)
+        return impl.getValue()[path]?.get(id.raw)
     }
 
     override fun setStateForId(path: PermissionPath, id: PermissionMapId, newState: BotPermissionState) {
-        map.updateAndGet { immutablePermissionsMap ->
+        impl.updateValue { immutablePermissionsMap ->
             immutablePermissionsMap.mutate { permissionsMap ->
                 val immutableIdMap = permissionsMap.getOrDefault(path, persistentMapOf())
                 permissionsMap[path] = immutableIdMap.mutate { idMap ->
