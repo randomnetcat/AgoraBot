ea2b8e2e88e4821629a27eb5c4ce5afc606a4912
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-14 23:26:19 -0400

    Add basic join/leave messages

--- src/main/kotlin/org/randomcat/agorabot/commands/SecretHitler.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/SecretHitler.kt
@@ -1,16 +1,62 @@
 package org.randomcat.agorabot.commands
 
-import org.randomcat.agorabot.commands.impl.BaseCommand
-import org.randomcat.agorabot.commands.impl.BaseCommandImplReceiver
-import org.randomcat.agorabot.commands.impl.BaseCommandStrategy
+import kotlinx.serialization.Serializable
+import org.randomcat.agorabot.buttons.ButtonRequestDescriptor
+import org.randomcat.agorabot.buttons.ButtonRequestId
+import org.randomcat.agorabot.commands.impl.*
+import org.randomcat.agorabot.permissions.GuildScope
+import org.randomcat.agorabot.secrethitler.SecretHitlerGameId
+import org.randomcat.agorabot.secrethitler.SecretHitlerGameState
 import org.randomcat.agorabot.secrethitler.SecretHitlerRepository
+import org.randomcat.agorabot.secrethitler.formatSecretHitlerJoinMessage
+import java.time.Duration
+
+private val MANAGE_PERMISSION = GuildScope.command("secret_hitler").action("manage")
+
+private val JOIN_LEAVE_DURATION = Duration.ofDays(1)
 
 class SecretHitlerCommand(
     strategy: BaseCommandStrategy,
     private val repository: SecretHitlerRepository,
 ) : BaseCommand(strategy) {
+    @Serializable
+    data class JoinGameRequestDescriptor(val gameId: SecretHitlerGameId) : ButtonRequestDescriptor
+
+    @Serializable
+    data class LeaveGameRequestDescriptor(val gameId: SecretHitlerGameId) : ButtonRequestDescriptor
+
     override fun BaseCommandImplReceiver.impl() {
         subcommands {
+            subcommand("start") {
+                noArgs().requiresGuild().permissions(MANAGE_PERMISSION) {
+                    val state = SecretHitlerGameState.Joining()
+                    val gameId = repository.gameList.createGame(state)
+                    val assignSucceeded = repository.channelGameMap.tryPutGameForChannelId(currentChannel().id, gameId)
+
+                    if (assignSucceeded) {
+                        respond(
+                            formatSecretHitlerJoinMessage(
+                                state = state,
+                                joinButtonId = ButtonRequestId(
+                                    newButtonId(
+                                        descriptor = JoinGameRequestDescriptor(gameId = gameId),
+                                        expiryDuration = JOIN_LEAVE_DURATION,
+                                    ),
+                                ),
+                                leaveButtonId = ButtonRequestId(
+                                    newButtonId(
+                                        descriptor = LeaveGameRequestDescriptor(gameId = gameId),
+                                        expiryDuration = JOIN_LEAVE_DURATION,
+                                    ),
+                                ),
+                            ),
+                        )
+                    } else {
+                        respond("There is already a game ongoing in this channel.")
+                        repository.gameList.removeGameIfExists(gameId)
+                    }
+                }
+            }
         }
     }
 }
---
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/Message.kt
@@ -0,0 +1,35 @@
+package org.randomcat.agorabot.secrethitler
+
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.MessageBuilder
+import net.dv8tion.jda.api.entities.MessageEmbed
+import net.dv8tion.jda.api.interactions.components.ActionRow
+import net.dv8tion.jda.api.interactions.components.Button
+import org.randomcat.agorabot.buttons.ButtonRequestId
+import org.randomcat.agorabot.util.DiscordMessage
+
+fun formatSecretHitlerJoinMessageEmbed(state: SecretHitlerGameState.Joining): MessageEmbed {
+    return EmbedBuilder()
+        .setTitle("Secret Hitler Game")
+        .addField(
+            "Players",
+            state.playerNames.joinToString("\n") { name -> "<@${name.raw}>" },
+            false,
+        )
+        .build()
+}
+
+fun formatSecretHitlerJoinMessage(
+    state: SecretHitlerGameState.Joining,
+    joinButtonId: ButtonRequestId,
+    leaveButtonId: ButtonRequestId,
+): DiscordMessage {
+    return MessageBuilder(formatSecretHitlerJoinMessageEmbed(state = state))
+        .setActionRows(
+            ActionRow.of(
+                Button.success(joinButtonId.raw, "Join"),
+                Button.danger(leaveButtonId.raw, "Leave"),
+            ),
+        )
+        .build()
+}
