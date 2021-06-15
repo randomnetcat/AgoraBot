7e4012029edbd4416cfae31f44974886ad86e369
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-15 17:53:22 -0400

    Have SecretHitlerPlayerMap use a map instead

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Player.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Player.kt
@@ -1,7 +1,8 @@
 package org.randomcat.agorabot.secrethitler.model
 
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.toImmutableList
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.toImmutableMap
+import org.randomcat.util.isDistinct
 
 @JvmInline
 value class SecretHitlerPlayerNumber(val raw: Int)
@@ -10,17 +11,25 @@ value class SecretHitlerPlayerNumber(val raw: Int)
 value class SecretHitlerPlayerExternalName(val raw: String)
 
 data class SecretHitlerPlayerMap(
-    private val players: ImmutableList<SecretHitlerPlayerExternalName>,
+    private val players: ImmutableMap<SecretHitlerPlayerNumber, SecretHitlerPlayerExternalName>,
 ) {
-    constructor(players: List<SecretHitlerPlayerExternalName>) : this(players.toImmutableList())
+    constructor(players: Map<SecretHitlerPlayerNumber, SecretHitlerPlayerExternalName>) : this(players.toImmutableMap())
+
+    init {
+        require(players.values.isDistinct())
+    }
 
     fun playerByNumber(number: SecretHitlerPlayerNumber): SecretHitlerPlayerExternalName {
-        return players[number.raw]
+        return players.getValue(number)
     }
 
     fun numberByPlayer(playerName: SecretHitlerPlayerExternalName): SecretHitlerPlayerNumber {
-        return SecretHitlerPlayerNumber(players.indexOf(playerName))
+        return players.entries.single { it.value == playerName }.key
     }
 
-    val validNumbers: Set<SecretHitlerPlayerNumber> = players.indices.map { SecretHitlerPlayerNumber(it) }.toSet()
+    fun toMap(): Map<SecretHitlerPlayerNumber, SecretHitlerPlayerExternalName> {
+        return players
+    }
+
+    val validNumbers: Set<SecretHitlerPlayerNumber> = players.keys
 }
