cdd8bd2d032d34512e509defb976f03c92c97019
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-15 14:44:11 -0400

    Add basic deck state

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/Model.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/Model.kt
@@ -72,6 +72,120 @@ fun SecretHitlerRoleMap.playerIsPlainFascist(player: SecretHitlerPlayerNumber) =
 fun SecretHitlerRoleMap.playerIsAnyFascist(player: SecretHitlerPlayerNumber) = allFascistPlayers.contains(player)
 fun SecretHitlerRoleMap.playerIsHitler(player: SecretHitlerPlayerNumber) = hitlerPlayer == player
 
+enum class SecretHitlerPolicyType {
+    FASCIST,
+    LIBERAL,
+}
+
+data class SecretHitlerDeckState(private val policies: ImmutableList<SecretHitlerPolicyType>) {
+    constructor(policies: List<SecretHitlerPolicyType>) : this(policies.toImmutableList())
+
+    init {
+        require(policies.size >= STANDARD_DRAW_AMOUNT)
+    }
+
+    interface ShuffleProvider {
+        fun newDeck(): SecretHitlerDeckState
+    }
+
+    object RandomShuffleProvider : ShuffleProvider {
+        override fun newDeck(): SecretHitlerDeckState {
+            return SecretHitlerDeckState.random()
+        }
+    }
+
+    companion object {
+        private const val FASCIST_COUNT = 11
+        private const val LIBERAL_COUNT = 6
+
+        fun random(): SecretHitlerDeckState {
+            return SecretHitlerDeckState(
+                @OptIn(ExperimentalStdlibApi::class)
+                buildList {
+                    repeat(FASCIST_COUNT) { add(SecretHitlerPolicyType.FASCIST) }
+                    repeat(LIBERAL_COUNT) { add(SecretHitlerPolicyType.LIBERAL) }
+                }.shuffled(),
+            )
+        }
+
+        const val STANDARD_DRAW_AMOUNT = 3
+    }
+
+    private data class DrawAnyResult(
+        val drawnCards: ImmutableList<SecretHitlerPolicyType>,
+        val newDeck: SecretHitlerDeckState,
+        val shuffled: Boolean,
+    ) {
+        fun asStandard(): StandardDrawResult {
+            return StandardDrawResult(
+                drawnCards = drawnCards,
+                newDeck = newDeck,
+                shuffled = shuffled,
+            )
+        }
+
+        fun asSingle(): SingleDrawResult {
+            return SingleDrawResult(
+                drawnCard = drawnCards.single(),
+                newDeck = newDeck,
+                shuffled = shuffled,
+            )
+        }
+    }
+
+    data class StandardDrawResult(
+        val drawnCards: ImmutableList<SecretHitlerPolicyType>,
+        val newDeck: SecretHitlerDeckState,
+        val shuffled: Boolean,
+    ) {
+        init {
+            require(drawnCards.size == STANDARD_DRAW_AMOUNT)
+        }
+    }
+
+    data class SingleDrawResult(
+        val drawnCard: SecretHitlerPolicyType,
+        val newDeck: SecretHitlerDeckState,
+        val shuffled: Boolean,
+    )
+
+    private fun drawAny(count: Int, shuffleProvider: ShuffleProvider): DrawAnyResult {
+        require(count > 0)
+        check(policies.size >= count)
+
+        val drawnCards = policies.subList(0, count)
+        val remainingSize = policies.size - count
+
+        return if (remainingSize < STANDARD_DRAW_AMOUNT) {
+            DrawAnyResult(
+                drawnCards = drawnCards,
+                newDeck = shuffleProvider.newDeck(),
+                shuffled = true,
+            )
+        } else {
+            val remainingCards = policies.subList(count, policies.size)
+
+            DrawAnyResult(
+                drawnCards = drawnCards,
+                newDeck = SecretHitlerDeckState(remainingCards),
+                shuffled = false,
+            )
+        }
+    }
+
+    fun drawStandard(shuffleProvider: ShuffleProvider): StandardDrawResult {
+        return drawAny(count = STANDARD_DRAW_AMOUNT, shuffleProvider).asStandard()
+    }
+
+    fun drawSingle(shuffleProvider: ShuffleProvider): SingleDrawResult {
+        return drawAny(count = 1, shuffleProvider).asSingle()
+    }
+}
+
+data class SecretHitlerBoardState(
+    val deckState: SecretHitlerDeckState,
+)
+
 sealed class SecretHitlerGameState {
     data class Joining(val playerNames: ImmutableSet<SecretHitlerPlayerExternalName>) : SecretHitlerGameState() {
         constructor() : this(persistentSetOf())
@@ -88,6 +202,7 @@ sealed class SecretHitlerGameState {
     data class Running(
         val playerMap: SecretHitlerPlayerMap,
         val roleMap: SecretHitlerRoleMap,
+        val boardState: SecretHitlerBoardState,
     ) {
         init {
             require(playerMap.validNumbers == roleMap.assignedPlayers) {
