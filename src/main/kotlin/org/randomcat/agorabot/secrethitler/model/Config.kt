6ca8098c50abc2e6243712dc6757a9e72645c230
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-20 17:08:25 -0400

    Add SecretHitlerStartConfiguration with role information

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Config.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Config.kt
@@ -14,3 +14,24 @@ data class SecretHitlerGameConfiguration(
         return fascistPowers[fascistPoliciesEnacted - 1]
     }
 }
+
+
+data class SecretHitlerRoleConfiguration(
+    val liberalCount: Int,
+    val plainFascistCount: Int,
+    val hitlerKnowsFascists: Boolean,
+) {
+    init {
+        require(liberalCount > 0)
+        require(plainFascistCount > 0)
+    }
+
+    // Liberals + Plain Fascists + 1 Hitler
+    val totalRoles
+        get() = liberalCount + plainFascistCount + 1
+}
+
+data class SecretHitlerStartConfiguration(
+    val roleConfiguration: SecretHitlerRoleConfiguration,
+    val gameConfiguration: SecretHitlerGameConfiguration,
+)
