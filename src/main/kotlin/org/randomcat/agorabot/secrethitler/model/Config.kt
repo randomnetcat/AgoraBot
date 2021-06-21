4e554cc37569a1adee470764fc2dc6b6d49c8b8b
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-21 13:03:41 -0400

    Add map of player numbers to start configurations

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Config.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Config.kt
@@ -1,6 +1,9 @@
 package org.randomcat.agorabot.secrethitler.model
 
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.persistentMapOf
+import kotlinx.collections.immutable.toImmutableList
 
 data class SecretHitlerGameConfiguration(
     val liberalWinRequirement: Int,
@@ -35,3 +38,92 @@ data class SecretHitlerStartConfiguration(
     val roleConfiguration: SecretHitlerRoleConfiguration,
     val gameConfiguration: SecretHitlerGameConfiguration,
 )
+
+
+private val POWERS_5_OR_6 = persistentListOf(
+    null,
+    null,
+    SecretHitlerFascistPower.EXAMINE_CARDS,
+    SecretHitlerFascistPower.EXECUTE_PLAYER,
+    SecretHitlerFascistPower.EXECUTE_PLAYER,
+)
+
+private val POWERS_7_OR_MORE = persistentListOf(
+    null,
+    SecretHitlerFascistPower.INVESTIGATE_PARTY,
+    SecretHitlerFascistPower.SPECIAL_ELECTION,
+    SecretHitlerFascistPower.EXECUTE_PLAYER,
+    SecretHitlerFascistPower.EXECUTE_PLAYER,
+)
+
+private const val LIBERAL_WIN_REQUIREMENT = 5
+private const val HITLER_CHANCELLOR_WIN_REQUIREMENT = 3
+private const val VETO_REQUIREMENT = 5
+
+private fun makeStandardGameConfiguration(
+    fascistPowers: List<SecretHitlerFascistPower?>,
+): SecretHitlerGameConfiguration {
+    return SecretHitlerGameConfiguration(
+        liberalWinRequirement = LIBERAL_WIN_REQUIREMENT,
+        fascistPowers = fascistPowers.toImmutableList(),
+        hitlerChancellorWinRequirement = HITLER_CHANCELLOR_WIN_REQUIREMENT,
+        vetoUnlockRequirement = VETO_REQUIREMENT,
+    )
+}
+
+private val GAME_CONFIG_MAP = persistentMapOf(
+    5 to SecretHitlerStartConfiguration(
+        SecretHitlerRoleConfiguration(
+            liberalCount = 3,
+            plainFascistCount = 1,
+            hitlerKnowsFascists = true,
+        ),
+        makeStandardGameConfiguration(fascistPowers = POWERS_5_OR_6),
+    ),
+    6 to SecretHitlerStartConfiguration(
+        SecretHitlerRoleConfiguration(
+            liberalCount = 4,
+            plainFascistCount = 1,
+            hitlerKnowsFascists = true,
+        ),
+        makeStandardGameConfiguration(fascistPowers = POWERS_5_OR_6),
+    ),
+    7 to SecretHitlerStartConfiguration(
+        SecretHitlerRoleConfiguration(
+            liberalCount = 4,
+            plainFascistCount = 2,
+            hitlerKnowsFascists = false,
+        ),
+        makeStandardGameConfiguration(fascistPowers = POWERS_7_OR_MORE),
+    ),
+    8 to SecretHitlerStartConfiguration(
+        SecretHitlerRoleConfiguration(
+            liberalCount = 5,
+            plainFascistCount = 2,
+            hitlerKnowsFascists = false,
+        ),
+        makeStandardGameConfiguration(fascistPowers = POWERS_7_OR_MORE),
+    ),
+    9 to SecretHitlerStartConfiguration(
+        SecretHitlerRoleConfiguration(
+            liberalCount = 5,
+            plainFascistCount = 3,
+            hitlerKnowsFascists = false,
+        ),
+        makeStandardGameConfiguration(fascistPowers = POWERS_7_OR_MORE),
+    ),
+    10 to SecretHitlerStartConfiguration(
+        SecretHitlerRoleConfiguration(
+            liberalCount = 6,
+            plainFascistCount = 3,
+            hitlerKnowsFascists = false,
+        ),
+        makeStandardGameConfiguration(fascistPowers = POWERS_7_OR_MORE),
+    ),
+)
+
+fun SecretHitlerGameState.Joining.startConfiguration(): SecretHitlerStartConfiguration {
+    return GAME_CONFIG_MAP.getValue(playerNames.size).also {
+        check(it.roleConfiguration.totalRoles == playerNames.size)
+    }
+}
