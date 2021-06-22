b3d659d884a242ae331d88441db21cecae79ee34
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-22 18:08:15 -0400

    Narrator voice: it was not, in fact, fine

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/Handlers.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/Handlers.kt
@@ -2,13 +2,11 @@ package org.randomcat.agorabot.secrethitler.handlers
 
 import org.randomcat.agorabot.commands.impl.BaseCommandExecutionReceiverGuilded
 import org.randomcat.agorabot.secrethitler.SecretHitlerRepository
-import org.randomcat.agorabot.secrethitler.handlers.SecretHitlerHandlers.CommandReceiver
 import org.randomcat.agorabot.secrethitler.model.SecretHitlerGameId
 
-object SecretHitlerHandlers {
-    @Suppress("TOPLEVEL_TYPEALIASES_ONLY") // It'll be fine
-    private typealias CommandReceiver = BaseCommandExecutionReceiverGuilded
+private typealias CommandReceiver = BaseCommandExecutionReceiverGuilded
 
+object SecretHitlerHandlers {
     private fun CommandReceiver.context() = SecretHitlerCommandContext(this)
 
     fun CommandReceiver.handleStart(
