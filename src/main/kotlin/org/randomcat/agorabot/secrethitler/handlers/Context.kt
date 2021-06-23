9dcf18ba316bf19f2c70df5e2da354d5bed20aed
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-22 22:06:13 -0400

    Add button context

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/Context.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/Context.kt
@@ -1,11 +1,25 @@
 package org.randomcat.agorabot.secrethitler.handlers
 
 import net.dv8tion.jda.api.interactions.Interaction
+import org.randomcat.agorabot.buttons.ButtonRequestDescriptor
 import org.randomcat.agorabot.commands.impl.BaseCommandExecutionReceiverGuilded
+import org.randomcat.agorabot.commands.impl.currentChannel
 import org.randomcat.agorabot.secrethitler.model.SecretHitlerPlayerExternalName
 import org.randomcat.agorabot.util.DiscordMessage
+import java.time.Duration
 
-interface SecretHitlerCommandContext {
+interface SecretHitlerButtonContext {
+    fun newButtonId(descriptor: ButtonRequestDescriptor, expiryDuration: Duration): String
+}
+
+interface SecretHitlerMessageContext {
+    fun sendGameMessage(message: String)
+    fun sendGameMessage(message: DiscordMessage)
+}
+
+interface SecretHitlerGameContext : SecretHitlerButtonContext, SecretHitlerMessageContext
+
+interface SecretHitlerCommandContext : SecretHitlerGameContext {
     fun respond(message: String)
     fun respond(message: DiscordMessage)
 }
@@ -18,6 +32,20 @@ fun SecretHitlerCommandContext(
     commandReceiver: BaseCommandExecutionReceiverGuilded,
 ): SecretHitlerCommandContext {
     return object : SecretHitlerCommandContext {
+        override fun newButtonId(descriptor: ButtonRequestDescriptor, expiryDuration: Duration): String {
+            return commandReceiver.newButtonId(descriptor, expiryDuration)
+        }
+
+        // Assume that commands are only sent in the same channel as the game.
+        override fun sendGameMessage(message: String) {
+            commandReceiver.currentChannel().sendMessage(message).queue()
+        }
+
+        // Assume that commands are only sent in the same channel as the game.
+        override fun sendGameMessage(message: DiscordMessage) {
+            commandReceiver.currentChannel().sendMessage(message).queue()
+        }
+
         override fun respond(message: DiscordMessage) {
             commandReceiver.respond(message)
         }
