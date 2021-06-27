26ec47be4dd8262c4c6691517c01c85e2c22e04c
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-27 00:01:22 -0400

    Add CommandNameContext to extract getting DM user ids

--- src/main/kotlin/org/randomcat/agorabot/features/SecretHitler.kt
+++ src/main/kotlin/org/randomcat/agorabot/features/SecretHitler.kt
@@ -8,6 +8,7 @@ import org.randomcat.agorabot.buttons.ButtonHandlerMap
 import org.randomcat.agorabot.buttons.withType
 import org.randomcat.agorabot.commands.SecretHitlerCommand
 import org.randomcat.agorabot.listener.Command
+import org.randomcat.agorabot.secrethitler.SecretHitlerImpersonationMap
 import org.randomcat.agorabot.secrethitler.SecretHitlerMutableImpersonationMap
 import org.randomcat.agorabot.secrethitler.SecretHitlerRepository
 import org.randomcat.agorabot.secrethitler.buttons.SecretHitlerChancellorCandidateSelectionButtonDescriptor
@@ -17,8 +18,11 @@ import org.randomcat.agorabot.secrethitler.handlers.SecretHitlerButtons
 import org.randomcat.agorabot.secrethitler.handlers.SecretHitlerInteractionContext
 import org.randomcat.agorabot.secrethitler.handlers.SecretHitlerNameContext
 import org.randomcat.agorabot.secrethitler.model.SecretHitlerPlayerExternalName
+import org.randomcat.agorabot.util.asSnowflakeOrNull
 
-private object NameContextImpl : SecretHitlerNameContext {
+private data class NameContextImpl(
+    val impersonationMap: SecretHitlerImpersonationMap?,
+) : SecretHitlerCommand.CommandNameContext {
     override fun renderExternalName(name: SecretHitlerPlayerExternalName): String {
         val raw = name.raw
 
@@ -28,25 +32,44 @@ private object NameContextImpl : SecretHitlerNameContext {
             raw
         }
     }
+
+    override fun resolveDmUserIds(
+        name: SecretHitlerPlayerExternalName,
+    ): SecretHitlerCommand.CommandNameContext.DmIdsResult {
+        val rawName = name.raw
+
+        val impersonationIds = impersonationMap?.dmUserIdsForName(rawName)
+        if (impersonationIds != null) {
+            return SecretHitlerCommand.CommandNameContext.DmIdsResult.Impersonated(impersonationIds)
+        }
+
+        if (rawName.asSnowflakeOrNull() != null) {
+            return SecretHitlerCommand.CommandNameContext.DmIdsResult.Direct(rawName)
+        }
+
+        return SecretHitlerCommand.CommandNameContext.DmIdsResult.Invalid
+    }
 }
 
 fun secretHitlerFeature(
     repository: SecretHitlerRepository,
     impersonationMap: SecretHitlerMutableImpersonationMap?,
 ) = object : Feature {
+    private val nameContext = NameContextImpl(impersonationMap)
+
     override fun commandsInContext(context: FeatureContext): Map<String, Command> {
         return mapOf(
             "secret_hitler" to SecretHitlerCommand(
                 context.defaultCommandStrategy,
                 repository = repository,
                 impersonationMap = impersonationMap,
-                nameContext = NameContextImpl,
+                nameContext = nameContext,
             ),
         )
     }
 
     override fun buttonData(): FeatureButtonData {
-        val nameContext = object : SecretHitlerInteractionContext, SecretHitlerNameContext by NameContextImpl {
+        val interactionContext = object : SecretHitlerInteractionContext, SecretHitlerNameContext by nameContext {
             override fun nameFromInteraction(interaction: Interaction): SecretHitlerPlayerExternalName {
                 val userId = interaction.user.id
                 val effectiveName = impersonationMap?.currentNameForId(userId) ?: userId
@@ -60,7 +83,7 @@ fun secretHitlerFeature(
                 withType<SecretHitlerJoinGameButtonDescriptor> { context, request ->
                     SecretHitlerButtons.handleJoin(
                         repository = repository,
-                        context = nameContext,
+                        context = interactionContext,
                         event = context.event,
                         request = request,
                     )
@@ -69,7 +92,7 @@ fun secretHitlerFeature(
                 withType<SecretHitlerLeaveGameButtonDescriptor> { context, request ->
                     SecretHitlerButtons.handleLeave(
                         repository = repository,
-                        context = nameContext,
+                        context = interactionContext,
                         event = context.event,
                         request = request,
                     )
@@ -78,7 +101,7 @@ fun secretHitlerFeature(
                 withType<SecretHitlerChancellorCandidateSelectionButtonDescriptor> { context, request ->
                     SecretHitlerButtons.handleChancellorSelection(
                         repository = repository,
-                        context = nameContext,
+                        context = interactionContext,
                         event = context.event,
                         request = request,
                     )
