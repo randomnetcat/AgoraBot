0c053a1687042dd58984804e84f5309534f1cabb
Jason Cobb <jason.e.cobb@gmail.com>
2021-06-27 13:24:52 -0400

    Add more vote data to VotingOngoing state

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Ephemeral.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Ephemeral.kt
@@ -1,7 +1,6 @@
 package org.randomcat.agorabot.secrethitler.model
 
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.toImmutableList
+import kotlinx.collections.immutable.*
 
 sealed class SecretHitlerEphemeralState {
     data class PresidentPolicyOptions(val policies: ImmutableList<SecretHitlerPolicyType>) {
@@ -31,15 +30,51 @@ sealed class SecretHitlerEphemeralState {
     ) : SecretHitlerEphemeralState() {
         fun selectChancellor(chancellor: SecretHitlerPlayerNumber): VotingOngoing {
             return VotingOngoing(
-                SecretHitlerGovernmentMembers(
+                governmentMembers = SecretHitlerGovernmentMembers(
                     president = presidentCandidate,
                     chancellor = chancellor,
-                )
+                ),
+                voteMap = VoteMap.empty(),
             )
         }
     }
 
-    data class VotingOngoing(val governmentMembers: SecretHitlerGovernmentMembers) : SecretHitlerEphemeralState()
+    enum class VoteKind {
+        FOR,
+        AGAINST,
+    }
+
+    data class VoteMap(private val votesByPlayer: ImmutableMap<SecretHitlerPlayerNumber, VoteKind>) {
+        constructor(votesByPlayer: Map<SecretHitlerPlayerNumber, VoteKind>) : this(votesByPlayer.toImmutableMap())
+
+        companion object {
+            private val EMPTY = VoteMap(persistentMapOf())
+            fun empty(): VoteMap = EMPTY
+        }
+
+        val votingPlayers: Set<SecretHitlerPlayerNumber> = votesByPlayer.keys
+
+        fun voteForPlayer(playerNumber: SecretHitlerPlayerNumber): VoteKind {
+            return votesByPlayer.getValue(playerNumber)
+        }
+
+        fun withVote(player: SecretHitlerPlayerNumber, vote: VoteKind): VoteMap {
+            require(!votesByPlayer.containsKey(player)) {
+                "Attempt to set duplicate vote for player with name ${player.raw}"
+            }
+
+            return VoteMap(votesByPlayer.toPersistentMap().put(player, vote))
+        }
+
+        fun toMap(): Map<SecretHitlerPlayerNumber, VoteKind> {
+            return votesByPlayer
+        }
+    }
+
+    data class VotingOngoing(
+        val governmentMembers: SecretHitlerGovernmentMembers,
+        val voteMap: VoteMap,
+    ) : SecretHitlerEphemeralState()
 
     data class PresidentPolicyChoicePending(
         val governmentMembers: SecretHitlerGovernmentMembers,
