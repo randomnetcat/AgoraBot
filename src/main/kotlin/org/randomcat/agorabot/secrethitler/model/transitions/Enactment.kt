49716ccf1f2582eb92360cb00fdc336b2adb10e2
Jason Cobb <jason.e.cobb@gmail.com>
2021-07-01 22:04:00 -0400

    Add enactment and speedy enactment transition helpers

---
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/model/transitions/Enactment.kt
@@ -0,0 +1,61 @@
+package org.randomcat.agorabot.secrethitler.model.transitions
+
+import org.randomcat.agorabot.secrethitler.model.SecretHitlerFascistPower
+import org.randomcat.agorabot.secrethitler.model.SecretHitlerGlobalGameState
+import org.randomcat.agorabot.secrethitler.model.SecretHitlerPoliciesState
+import org.randomcat.agorabot.secrethitler.model.SecretHitlerPolicyType
+
+sealed class SecretHitlerEnactmentResult {
+    sealed class GameContinues : SecretHitlerEnactmentResult() {
+        data class NoPower(val newGlobalState: SecretHitlerGlobalGameState) : GameContinues()
+
+        data class PowerActivated(
+            val newGlobalState: SecretHitlerGlobalGameState,
+            val power: SecretHitlerFascistPower,
+        ) : GameContinues()
+    }
+
+    data class GameEnds(val winResult: SecretHitlerWinResult) : SecretHitlerEnactmentResult()
+}
+
+private fun SecretHitlerGlobalGameState.withPoliciesState(
+    newPoliciesState: SecretHitlerPoliciesState,
+): SecretHitlerGlobalGameState {
+    return this.copy(
+        boardState = this.boardState.copy(
+            policiesState = newPoliciesState,
+        ),
+    )
+}
+
+fun SecretHitlerGlobalGameState.afterEnacting(policyType: SecretHitlerPolicyType): SecretHitlerEnactmentResult {
+    val nestedResult = this.boardState.policiesState.withPolicyEnacted(
+        config = this.configuration,
+        type = policyType,
+    )
+
+    return when (nestedResult) {
+        is SecretHitlerPoliciesState.EnactmentResult.GameContinues -> {
+            val newGlobalState = this.withPoliciesState(nestedResult.newPolicyState)
+
+            if (nestedResult.fascistPower != null) {
+                SecretHitlerEnactmentResult.GameContinues.PowerActivated(
+                    newGlobalState = newGlobalState,
+                    power = nestedResult.fascistPower,
+                )
+            } else {
+                SecretHitlerEnactmentResult.GameContinues.NoPower(
+                    newGlobalState = newGlobalState,
+                )
+            }
+        }
+
+        is SecretHitlerPoliciesState.EnactmentResult.ImmediateWin.LiberalWin -> {
+            SecretHitlerEnactmentResult.GameEnds(SecretHitlerWinResult.LiberalsWin)
+        }
+
+        is SecretHitlerPoliciesState.EnactmentResult.ImmediateWin.FascistWin -> {
+            SecretHitlerEnactmentResult.GameEnds(SecretHitlerWinResult.FascistsWin)
+        }
+    }
+}
