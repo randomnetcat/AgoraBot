091c38e3572c7c1daa38d4e5442f31cd43864181
Jason Cobb <jason.e.cobb@gmail.com>
2021-07-04 15:10:29 -0400

    Send basic notification on rejected government

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/GovernmentNotifications.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/GovernmentNotifications.kt
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.interactions.components.ActionRow
 import net.dv8tion.jda.api.interactions.components.Button
 import org.randomcat.agorabot.secrethitler.buttons.SecretHitlerPresidentPolicyChoiceButtonDescriptor
 import org.randomcat.agorabot.secrethitler.model.*
+import org.randomcat.agorabot.secrethitler.model.transitions.SecretHitlerInactiveGovernmentResult
 import org.randomcat.agorabot.util.MAX_BUTTONS_PER_ROW
 import java.time.Duration
 
@@ -65,33 +66,49 @@ private fun sendPresidentPolicySelectionMessage(
     )
 }
 
-private fun sendElectedNotification(
+private fun sendBasicElectionNotification(
     context: SecretHitlerGameContext,
-    currentState: SecretHitlerGameState.Running.With<SecretHitlerEphemeralState.PresidentPolicyChoicePending>,
+    title: String,
+    description: String,
+    playerMap: SecretHitlerPlayerMap,
+    governmentMembers: SecretHitlerGovernmentMembers,
 ) {
     fun renderNameForNumber(number: SecretHitlerPlayerNumber): String {
-        return context.renderExternalName(currentState.globalState.playerMap.playerByNumberKnown(number))
+        return context.renderExternalName(playerMap.playerByNumberKnown(number))
     }
 
     context.sendGameMessage(
         MessageBuilder()
             .setEmbed(
                 EmbedBuilder()
-                    .setTitle("Government Elected")
-                    .appendDescription("The government has been elected. Policy selection will now commence.")
+                    .setTitle(title)
+                    .appendDescription(description)
                     .addField(
                         "President",
-                        renderNameForNumber(currentState.ephemeralState.governmentMembers.president),
+                        renderNameForNumber(governmentMembers.president),
                         true,
                     )
                     .addField(
                         "Chancellor",
-                        renderNameForNumber(currentState.ephemeralState.governmentMembers.chancellor),
+                        renderNameForNumber(governmentMembers.chancellor),
                         true,
                     )
                     .build(),
             )
             .build(),
+    )
+}
+
+private fun sendElectedNotification(
+    context: SecretHitlerGameContext,
+    currentState: SecretHitlerGameState.Running.With<SecretHitlerEphemeralState.PresidentPolicyChoicePending>,
+) {
+    sendBasicElectionNotification(
+        context = context,
+        title = "Government Elected",
+        description = "The government has been elected. Policy selection will now commence.",
+        playerMap = currentState.globalState.playerMap,
+        governmentMembers = currentState.ephemeralState.governmentMembers,
     )
 }
 
@@ -107,4 +124,35 @@ fun sendSecretHitlerGovernmentElectedMessages(
         currentState = currentState,
         gameId = gameId,
     )
+}
+
+
+private fun sendGovernmentRejectedNotification(
+    context: SecretHitlerGameContext,
+    playerMap: SecretHitlerPlayerMap,
+    governmentMembers: SecretHitlerGovernmentMembers,
+) {
+    sendBasicElectionNotification(
+        context = context,
+        title = "Government Rejected",
+        description = "The government has been rejected. The election tracker has been incremented by 1.",
+        playerMap = playerMap,
+        governmentMembers = governmentMembers,
+    )
+}
+
+fun sendSecretHitlerGovernmentRejectedMessages(
+    context: SecretHitlerGameContext,
+    gameId: SecretHitlerGameId,
+    playerMap: SecretHitlerPlayerMap,
+    governmentMembers: SecretHitlerGovernmentMembers,
+    result: SecretHitlerInactiveGovernmentResult,
+) {
+    sendGovernmentRejectedNotification(
+        context = context,
+        playerMap = playerMap,
+        governmentMembers = governmentMembers,
+    )
+
+    context.sendGameMessage("Futher messages not yet implemented")
 }
