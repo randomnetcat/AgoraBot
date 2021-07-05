8c747ba5f3278152460493068c2ba0632daefd66
Jason Cobb <jason.e.cobb@gmail.com>
2021-07-04 23:18:49 -0400

    Make election tracker transitions public and operate on global state

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/model/transitions/InactiveGovernment.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/model/transitions/InactiveGovernment.kt
@@ -23,34 +23,6 @@ sealed class SecretHitlerInactiveGovernmentResult {
     }
 }
 
-private fun <E : SecretHitlerEphemeralState> SecretHitlerGameState.Running.With<E>.withNewTrackerValue(
-    newTrackerState: Int,
-): SecretHitlerGameState.Running.With<E> {
-    return withGlobal(
-        newGlobalState = this.globalState.copy(
-            electionState = this.globalState.electionState.copy(
-                electionTrackerState = newTrackerState,
-            ),
-        ),
-    )
-}
-
-private fun <E : SecretHitlerEphemeralState> SecretHitlerGameState.Running.With<E>.withTrackerReset(
-): SecretHitlerGameState.Running.With<E> {
-    return withNewTrackerValue(0)
-}
-
-private fun <E : SecretHitlerEphemeralState> SecretHitlerGameState.Running.With<E>.withIncrementedElectionTracker(
-): SecretHitlerGameState.Running.With<E> {
-    val oldTrackerState = this.globalState.electionState.electionTrackerState
-    require(oldTrackerState != Int.MAX_VALUE)
-
-    val newTrackerState = oldTrackerState + 1
-    check(newTrackerState < this.globalState.configuration.speedyEnactRequirement)
-
-    return withNewTrackerValue(newTrackerState)
-}
-
 private fun <E : SecretHitlerEphemeralState> SecretHitlerGameState.Running.With<E>.withNoTermLimits(
 ): SecretHitlerGameState.Running.With<E> {
     return this.withGlobal(
@@ -71,9 +43,8 @@ private fun SecretHitlerGameState.Running.afterChaos(
         is SecretHitlerSpeedyEnactResult.GameContinues -> {
             SecretHitlerInactiveGovernmentResult.CountryInChaos.GameContinues(
                 newState = this
-                    .withGlobal(enactResult.newGlobalState)
+                    .withGlobal(enactResult.newGlobalState.withElectionTrackerReset())
                     .afterAdvancingTickerAndNewElection()
-                    .withTrackerReset()
                     .withNoTermLimits(),
                 drawnPolicyType = drawResult.drawnCard,
                 shuffledDeck = drawResult.shuffled,
@@ -97,7 +68,9 @@ fun SecretHitlerGameState.Running.afterInactiveGovernment(
     }
 
     return SecretHitlerInactiveGovernmentResult.NewElection(
-        newState = this.afterAdvancingTickerAndNewElection().withIncrementedElectionTracker(),
+        newState = this.afterAdvancingTickerAndNewElection().let {
+            it.withGlobal(it.globalState.withIncrementedElectionTracker())
+        },
     )
 }
 
