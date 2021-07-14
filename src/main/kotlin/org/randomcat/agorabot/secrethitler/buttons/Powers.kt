087a458fb880ee8cb1dc3748c26e3216a90a7479
Jason Cobb <jason.e.cobb@gmail.com>
2021-07-14 18:21:15 -0400

    Remove redundant president data from policy choice buttons

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/buttons/Powers.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/buttons/Powers.kt
@@ -8,20 +8,17 @@ import org.randomcat.agorabot.secrethitler.model.SecretHitlerPlayerNumber
 @Serializable
 data class SecretHitlerPendingInvestigatePartySelectionButtonDescriptor(
     val gameId: SecretHitlerGameId,
-    val president: SecretHitlerPlayerNumber,
     val selectedPlayer: SecretHitlerPlayerNumber,
 ) : ButtonRequestDescriptor
 
 @Serializable
 data class SecretHitlerPendingSpecialElectionSelectionButtonDescriptor(
     val gameId: SecretHitlerGameId,
-    val president: SecretHitlerPlayerNumber,
     val selectedPlayer: SecretHitlerPlayerNumber,
 ) : ButtonRequestDescriptor
 
 @Serializable
 data class SecretHitlerPendingExecutionSelectionButtonDescriptor(
     val gameId: SecretHitlerGameId,
-    val president: SecretHitlerPlayerNumber,
     val selectedPlayer: SecretHitlerPlayerNumber,
 ) : ButtonRequestDescriptor
