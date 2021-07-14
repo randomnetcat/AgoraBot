37183209e454e78083a353706ae316a759d52baa
Jason Cobb <jason.e.cobb@gmail.com>
2021-07-14 16:10:16 -0400

    Handle Chancellor selecting a policy

---
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/ChancellorPolicySelected.kt
@@ -0,0 +1,253 @@
+package org.randomcat.agorabot.secrethitler.handlers
+
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.MessageBuilder
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent
+import org.randomcat.agorabot.secrethitler.SecretHitlerRepository
+import org.randomcat.agorabot.secrethitler.buttons.SecretHitlerChancellorPolicyChoiceButtonDescriptor
+import org.randomcat.agorabot.secrethitler.model.*
+import org.randomcat.agorabot.secrethitler.model.transitions.SecretHitlerAfterChancellorPolicySelectedResult
+import org.randomcat.agorabot.secrethitler.model.transitions.afterChancellorPolicySelected
+import org.randomcat.agorabot.secrethitler.updateRunningGameWithValidExtract
+import org.randomcat.agorabot.util.handleTextResponse
+import org.randomcat.agorabot.secrethitler.model.SecretHitlerEphemeralState as EphemeralState
+import org.randomcat.agorabot.secrethitler.model.SecretHitlerGameState as GameState
+
+private sealed class ChancellorPolicySelectedHandlerUpdateResult {
+    data class Success(
+        val nestedResult: SecretHitlerAfterChancellorPolicySelectedResult,
+        val originalState: GameState.Running.With<EphemeralState.ChancellorPolicyChoicePending>,
+    ) : ChancellorPolicySelectedHandlerUpdateResult()
+
+    sealed class Failure : ChancellorPolicySelectedHandlerUpdateResult()
+    object NoSuchGame : Failure()
+    object NotPlayer : Failure()
+    object Unauthorized : Failure()
+    object InvalidState : Failure()
+}
+
+private fun doStateUpdate(
+    repository: SecretHitlerRepository,
+    gameId: SecretHitlerGameId,
+    actualChancellorName: SecretHitlerPlayerExternalName,
+    expectedChancellorNumber: SecretHitlerPlayerNumber,
+    selectedPolicyIndex: Int,
+): ChancellorPolicySelectedHandlerUpdateResult {
+    return repository.gameList.updateRunningGameWithValidExtract(
+        id = gameId,
+        onNoSuchGame = {
+            ChancellorPolicySelectedHandlerUpdateResult.NoSuchGame
+        },
+        onInvalidType = {
+            ChancellorPolicySelectedHandlerUpdateResult.InvalidState
+        },
+        validMapper = { currentState: GameState.Running.With<EphemeralState.ChancellorPolicyChoicePending> ->
+            val actualChancellorNumber = currentState.globalState.playerMap.numberByPlayer(actualChancellorName)
+            if (actualChancellorNumber == null) {
+                return@updateRunningGameWithValidExtract currentState to ChancellorPolicySelectedHandlerUpdateResult.NotPlayer
+            }
+
+            if (actualChancellorNumber != expectedChancellorNumber) {
+                return@updateRunningGameWithValidExtract currentState to ChancellorPolicySelectedHandlerUpdateResult.Unauthorized
+            }
+
+            check(actualChancellorNumber == currentState.ephemeralState.governmentMembers.chancellor)
+
+            val nestedResult = currentState.afterChancellorPolicySelected(policyIndex = selectedPolicyIndex)
+
+            val newState = when (nestedResult) {
+                is SecretHitlerAfterChancellorPolicySelectedResult.GameContinues -> {
+                    nestedResult.newState
+                }
+
+                is SecretHitlerAfterChancellorPolicySelectedResult.GameEnds -> {
+                    GameState.Completed
+                }
+            }
+
+            newState to ChancellorPolicySelectedHandlerUpdateResult.Success(
+                nestedResult = nestedResult,
+                originalState = currentState,
+            )
+        },
+        afterValid = { result ->
+            result
+        },
+    )
+}
+
+private fun sendPolicyEnactedNotification(
+    context: SecretHitlerGameContext,
+    policyType: SecretHitlerPolicyType,
+    power: SecretHitlerFascistPower?,
+) {
+    context.sendGameMessage(
+        MessageBuilder(
+            EmbedBuilder()
+                .setTitle("Policy enacted")
+                .addField(
+                    "Policy kind",
+                    policyType.readableName,
+                    true
+                )
+                .addField(
+                    "Power activated",
+                    power?.readableName ?: "[None]",
+                    true,
+                )
+                .build()
+        ).build()
+    )
+}
+
+private fun ChancellorPolicySelectedHandlerUpdateResult.Success.effectivePower(): SecretHitlerFascistPower? {
+    return when (nestedResult) {
+        is SecretHitlerAfterChancellorPolicySelectedResult.GameContinues.WithPower -> {
+            nestedResult.power
+        }
+
+        is SecretHitlerAfterChancellorPolicySelectedResult.GameContinues.NoPower -> {
+            null
+        }
+        is SecretHitlerAfterChancellorPolicySelectedResult.GameEnds -> {
+            null
+        }
+    }
+}
+
+
+fun doHandleSecretHitlerChancellorPolicySelected(
+    repository: SecretHitlerRepository,
+    context: SecretHitlerInteractionContext,
+    event: ButtonClickEvent,
+    request: SecretHitlerChancellorPolicyChoiceButtonDescriptor,
+) {
+    handleTextResponse(event) {
+        val gameId = request.gameId
+
+        val updateResult = doStateUpdate(
+            repository = repository,
+            gameId = gameId,
+            actualChancellorName = context.nameFromInteraction(event.interaction),
+            expectedChancellorNumber = request.chancellor,
+            selectedPolicyIndex = request.policyIndex,
+        )
+
+        when (updateResult) {
+            is ChancellorPolicySelectedHandlerUpdateResult.Success -> {
+                sendPolicyEnactedNotification(
+                    context = context,
+                    policyType = updateResult.nestedResult.policyType,
+                    power = updateResult.effectivePower(),
+                )
+
+                fun sendElectionStartMessage(
+                    newState: GameState.Running.With<EphemeralState.ChancellorSelectionPending>,
+                ) {
+                    secretHitlerSendChancellorSelectionMessage(
+                        context = context,
+                        gameId = gameId,
+                        state = newState,
+                    )
+                }
+
+                @Suppress("UNUSED_VARIABLE")
+                val ensureExhaustive = when (updateResult.nestedResult) {
+                    is SecretHitlerAfterChancellorPolicySelectedResult.GameContinues.NoPower -> {
+                        sendElectionStartMessage(newState = updateResult.nestedResult.newState)
+                    }
+
+                    is SecretHitlerAfterChancellorPolicySelectedResult.GameContinues.WithPower.Stateful -> {
+                        sendSecretHitlerPowerActivatedMessages(
+                            context = context,
+                            gameId = gameId,
+                            currentState = updateResult.nestedResult.newState,
+                        )
+                    }
+
+                    is SecretHitlerAfterChancellorPolicySelectedResult.GameContinues.WithPower.Stateless -> {
+                        val ensureExhaustive = when (updateResult.nestedResult.powerResult) {
+                            SecretHitlerAfterChancellorPolicySelectedResult.StatelessPowerKind.PolicyPeek -> {
+                                sendPolicyPeekMessages(
+                                    context = context,
+                                    gameId = gameId,
+                                    playerMap = updateResult.originalState.globalState.playerMap,
+                                    enactingGovernment = updateResult.originalState.ephemeralState.governmentMembers,
+                                    deckState = updateResult.originalState.globalState.boardState.deckState,
+                                )
+                            }
+                        }
+
+                        sendElectionStartMessage(newState = updateResult.nestedResult.newState)
+                    }
+
+                    is SecretHitlerAfterChancellorPolicySelectedResult.GameEnds -> {
+                        sendSecretHitlerWinMessage(
+                            context = context,
+                            winResult = updateResult.nestedResult.winResult,
+                        )
+                    }
+                }
+
+                "That policy has been enacted."
+            }
+
+            is ChancellorPolicySelectedHandlerUpdateResult.Unauthorized -> {
+                "You are not the Chancellor in that game."
+            }
+
+            is ChancellorPolicySelectedHandlerUpdateResult.NotPlayer -> {
+                "You are not a player in that game."
+            }
+
+            is ChancellorPolicySelectedHandlerUpdateResult.InvalidState -> {
+                "You can no longer select a policy in that game."
+            }
+
+            is ChancellorPolicySelectedHandlerUpdateResult.NoSuchGame -> {
+                "That game no longer exists."
+            }
+        }
+    }
+}
+
+private fun sendPolicyPeekMessages(
+    context: SecretHitlerInteractionContext,
+    gameId: SecretHitlerGameId,
+    playerMap: SecretHitlerPlayerMap,
+    enactingGovernment: SecretHitlerGovernmentMembers,
+    deckState: SecretHitlerDeckState,
+) {
+    val peekResult = deckState.peekStandard()
+
+    val presidentName = playerMap.playerByNumberKnown(enactingGovernment.president)
+
+    context.sendPrivateMessage(
+        recipient = presidentName,
+        gameId = gameId,
+        message = MessageBuilder(
+            EmbedBuilder()
+                .setTitle("Policy Peek")
+                .setDescription(
+                    peekResult
+                        .peekedCards
+                        .mapIndexed { index, policyType ->
+                            "Policy #${index + 1}: ${policyType.readableName}"
+                        }
+                        .joinToString("\n"),
+                )
+                .build(),
+        ).build(),
+    )
+
+    context.sendGameMessage(
+        MessageBuilder(
+            EmbedBuilder()
+                .setTitle("Policy Peek")
+                .setDescription(
+                    "${context.renderExternalName(presidentName)} has been shown the top ${peekResult.peekedCards.size} policies."
+                )
+                .build(),
+        ).build(),
+    )
+}
---
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/Powers.kt
@@ -0,0 +1,138 @@
+package org.randomcat.agorabot.secrethitler.handlers
+
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.MessageBuilder
+import net.dv8tion.jda.api.interactions.components.ActionRow
+import net.dv8tion.jda.api.interactions.components.Button
+import org.randomcat.agorabot.buttons.BUTTON_INVALID_ID_RAW
+import org.randomcat.agorabot.buttons.ButtonRequestDescriptor
+import org.randomcat.agorabot.secrethitler.buttons.SecretHitlerPendingExecutionSelectionButtonDescriptor
+import org.randomcat.agorabot.secrethitler.buttons.SecretHitlerPendingInvestigatePartySelectionButtonDescriptor
+import org.randomcat.agorabot.secrethitler.buttons.SecretHitlerPendingSpecialElectionSelectionButtonDescriptor
+import org.randomcat.agorabot.secrethitler.model.*
+import org.randomcat.agorabot.util.MAX_BUTTONS_PER_ROW
+import java.time.Duration
+
+private val BUTTON_EXPIRY = Duration.ofDays(1)
+
+private fun sendPlayerSelectPowerNotification(
+    context: SecretHitlerGameContext,
+    powerName: String,
+    description: String,
+    selectVerb: String,
+    presidentNumber: SecretHitlerPlayerNumber,
+    playerMap: SecretHitlerPlayerMap,
+    makeButtonDescriptor: (selectedPlayer: SecretHitlerPlayerNumber) -> ButtonRequestDescriptor,
+) {
+    val presidentName = playerMap.playerByNumberKnown(presidentNumber)
+
+    context.sendGameMessage(
+        MessageBuilder(
+            EmbedBuilder()
+                .setTitle(powerName)
+                .setDescription(description)
+                .addField(
+                    "President",
+                    context.renderExternalName(presidentName),
+                    false,
+                )
+                .build(),
+        )
+            .also { builder ->
+                val buttons = playerMap.validNumbers.sortedBy { it.raw }.mapIndexed { index, playerNumber ->
+                    val isPermissible = playerNumber != presidentNumber
+
+                    Button
+                        .primary(
+                            if (isPermissible) {
+                                context.newButtonId(
+                                    descriptor = makeButtonDescriptor(
+                                        playerNumber,
+                                    ),
+                                    expiryDuration = BUTTON_EXPIRY,
+                                )
+                            } else {
+                                BUTTON_INVALID_ID_RAW
+                            },
+                            "$selectVerb player ${index + 1}"
+                        )
+                        .withDisabled(!isPermissible)
+                }
+
+                builder.setActionRows(buttons.chunked(MAX_BUTTONS_PER_ROW) { ActionRow.of(it) })
+            }
+            .build(),
+    )
+}
+
+fun sendSecretHitlerPowerActivatedMessages(
+    context: SecretHitlerGameContext,
+    gameId: SecretHitlerGameId,
+    currentState: SecretHitlerGameState.Running.With<SecretHitlerEphemeralState.PolicyPending>,
+) {
+    val presidentNumber = currentState.ephemeralState.presidentNumber
+
+    fun doSendSelectMessage(
+        powerName: String,
+        powerDescription: String,
+        selectVerb: String,
+        makeButtonDescriptor: (playerNumber: SecretHitlerPlayerNumber) -> ButtonRequestDescriptor,
+    ) {
+        sendPlayerSelectPowerNotification(
+            context = context,
+            presidentNumber = presidentNumber,
+            playerMap = currentState.globalState.playerMap,
+            powerName = powerName,
+            description = powerDescription,
+            selectVerb = selectVerb,
+            makeButtonDescriptor = makeButtonDescriptor,
+        )
+    }
+
+    return when (currentState.ephemeralState) {
+        is SecretHitlerEphemeralState.PolicyPending.InvestigateParty -> {
+            doSendSelectMessage(
+                powerName = SecretHitlerFascistPower.INVESTIGATE_PARTY.readableName,
+                powerDescription = "The President must select a player's party to investigate.",
+                selectVerb = "Investigate",
+                makeButtonDescriptor = { selectedPlayer ->
+                    SecretHitlerPendingInvestigatePartySelectionButtonDescriptor(
+                        gameId = gameId,
+                        president = presidentNumber,
+                        selectedPlayer = selectedPlayer,
+                    )
+                },
+            )
+        }
+
+        is SecretHitlerEphemeralState.PolicyPending.SpecialElection -> {
+            doSendSelectMessage(
+                powerName = SecretHitlerFascistPower.SPECIAL_ELECTION.readableName,
+                powerDescription = "The President must select a Presidential candidate for a Special Election.",
+                selectVerb = "Nominate",
+                makeButtonDescriptor = { selectedPlayer ->
+                    SecretHitlerPendingSpecialElectionSelectionButtonDescriptor(
+                        gameId = gameId,
+                        president = presidentNumber,
+                        selectedPlayer = selectedPlayer,
+                    )
+                },
+            )
+        }
+
+        is SecretHitlerEphemeralState.PolicyPending.Execution -> {
+            doSendSelectMessage(
+                powerName = SecretHitlerFascistPower.EXECUTE_PLAYER.readableName,
+                powerDescription = "The President must select a player to kill.",
+                selectVerb = "Execute",
+                makeButtonDescriptor = { selectedPlayer ->
+                    SecretHitlerPendingExecutionSelectionButtonDescriptor(
+                        gameId = gameId,
+                        president = presidentNumber,
+                        selectedPlayer = selectedPlayer,
+                    )
+                },
+            )
+        }
+    }
+}
