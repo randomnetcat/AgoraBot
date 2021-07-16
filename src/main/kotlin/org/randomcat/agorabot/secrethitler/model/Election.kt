184b0d37a38759bce1623ea03c6654d7b4b98ee2
Jason Cobb <jason.e.cobb@gmail.com>
2021-07-15 22:39:42 -0400

    Add documentation for currentPresidentTicker

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Election.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/model/Election.kt
@@ -15,6 +15,14 @@ data class SecretHitlerTermLimitState(
 }
 
 data class SecretHitlerElectionState(
+    /**
+     * The ticker has the player number of the President for whom the most recent non-special election has begun.
+     *
+     * Specifically, for the first Presidential candidate, it is initialized to that candidate's number. When a normal
+     * election begins after that, the ticker is incremented (wrapping around if it would then exceed the highest player
+     * number), then the resulting player is the new Presidential candidate. For a special election, the state is moved
+     * to an election without updating the ticker; for the next normal election, the normal rotation will resume.
+     */
     val currentPresidentTicker: SecretHitlerPlayerNumber,
     val termLimitState: SecretHitlerTermLimitState,
     val electionTrackerState: Int,
