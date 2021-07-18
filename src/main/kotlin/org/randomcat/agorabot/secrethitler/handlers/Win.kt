31e305d1b0cc9165f06313fda5c9a9c257ad55b8
Jason Cobb <jason.e.cobb@gmail.com>
2021-07-17 23:08:46 -0400

    Add win condition to game over message

--- src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/Win.kt
+++ src/main/kotlin/org/randomcat/agorabot/secrethitler/handlers/Win.kt
@@ -4,6 +4,25 @@ import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.MessageBuilder
 import org.randomcat.agorabot.secrethitler.model.transitions.SecretHitlerWinResult
 
+private val SecretHitlerWinResult.winMessage: String
+    get() = when (this) {
+        SecretHitlerWinResult.FascistsWin.FascistPolicyGoalReached -> {
+            "The Fascist policy goal has been reached."
+        }
+
+        SecretHitlerWinResult.FascistsWin.HitlerElectedChancellor -> {
+            "Hitler has been elected chancellor after a sufficient number of Fascist policies were enacted."
+        }
+
+        SecretHitlerWinResult.LiberalsWin.HitlerKilled -> {
+            "Hitler has been killed."
+        }
+
+        SecretHitlerWinResult.LiberalsWin.LiberalPolicyGoalReached -> {
+            "The Liberal policy goal has been reached."
+        }
+    }
+
 fun sendSecretHitlerWinMessage(
     context: SecretHitlerGameContext,
     winResult: SecretHitlerWinResult,
@@ -14,11 +33,14 @@ fun sendSecretHitlerWinMessage(
                 .setTitle(
                     "Game Over",
                 )
-                .appendDescription(
+                .setDescription(winResult.winMessage)
+                .addField(
+                    "Result",
                     when (winResult) {
                         is SecretHitlerWinResult.LiberalsWin -> "Liberals win."
                         is SecretHitlerWinResult.FascistsWin -> "Fascists win."
                     },
+                    true,
                 )
                 .build(),
         ).build(),
