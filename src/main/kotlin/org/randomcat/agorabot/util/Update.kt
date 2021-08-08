bb3c49b422e2a4fa58f493944af59e977b107928
Jason Cobb <jason.e.cobb@gmail.com>
2021-08-07 23:56:51 -0400

    Use object instead of property in doUpdateAndExtract

--- src/main/kotlin/org/randomcat/agorabot/util/Update.kt
+++ src/main/kotlin/org/randomcat/agorabot/util/Update.kt
@@ -1,11 +1,10 @@
 package org.randomcat.agorabot.util
 
 @PublishedApi
-@JvmField
-internal val UNSET_UPDATED_VALUE = Any()
+internal object UpdateAndExtractUninit
 
 inline fun <T, V> doUpdateAndExtract(runUpdate: ((T) -> T) -> Unit, crossinline mapper: (T) -> Pair<T, V>): V {
-    var extractedValue: Any? = UNSET_UPDATED_VALUE
+    var extractedValue: Any? = UpdateAndExtractUninit
 
     runUpdate {
         val result = mapper(it)
@@ -16,6 +15,6 @@ inline fun <T, V> doUpdateAndExtract(runUpdate: ((T) -> T) -> Unit, crossinline 
     // Protect against a hostile runUpdate trying to change this with a race
     val finalValue = extractedValue
 
-    check(finalValue !== UNSET_UPDATED_VALUE)
+    check(finalValue !== UpdateAndExtractUninit)
     return finalValue as V
 }
