a5351cdaa6420b44bf2699417f7a5fccc4fe829d
Jason Cobb <jason.e.cobb@gmail.com>
2021-08-07 23:57:43 -0400

    Suppress warning in doUpdateAndExtract

--- src/main/kotlin/org/randomcat/agorabot/util/Update.kt
+++ src/main/kotlin/org/randomcat/agorabot/util/Update.kt
@@ -16,5 +16,9 @@ inline fun <T, V> doUpdateAndExtract(runUpdate: ((T) -> T) -> Unit, crossinline 
     val finalValue = extractedValue
 
     check(finalValue !== UpdateAndExtractUninit)
+
+    @Suppress("UNCHECKED_CAST")
+    // This is known to be safe. It must either be a V or UpdateAndExtractUninit, and it's been checked to not be the
+    // latter.
     return finalValue as V
 }
