173729c97631911e5e46f1bb7522adc5438b380d
Jason Cobb <jason.e.cobb@gmail.com>
2021-09-04 15:39:57 -0400

    Bump JDA

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
     implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.4")
-    implementation("net.dv8tion:JDA:4.2.1_269")
+    implementation("net.dv8tion:JDA:4.3.0_315")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
     implementation("org.kitteh.irc:client-lib:8.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.0")
