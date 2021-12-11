62291d15b98ba1732537996896138cc83cc4cb13
Jason Cobb <jason.e.cobb@gmail.com>
2021-12-11 00:44:03 -0500

    Reorganize dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,18 +19,23 @@ repositories {
 }
 
 dependencies {
+    // Updated with Kotlin version
     implementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.2")
+
+    // Other
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.4")
     implementation("net.dv8tion:JDA:4.3.0_339")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
     implementation("org.kitteh.irc:client-lib:8.0.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.2")
     implementation("com.github.ajalt.clikt:clikt:3.1.0")
     implementation("org.randomcat:kotlin-utils:2.0.1")
     implementation("jakarta.json:jakarta.json-api:2.0.1")
+
     runtimeOnly("org.glassfish:jakarta.json:2.0.1")
     runtimeOnly("org.slf4j:slf4j-simple:1.7.30")
 
+    // Testing
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit5"))
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.5.2")
