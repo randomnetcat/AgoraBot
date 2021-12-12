042da851f6f3c6d4a53772ca1ae63e2a5b93aeca
Jason Cobb <jason.e.cobb@gmail.com>
2021-12-12 18:40:25 -0500

    Use flake-compat for default.nix

--- default.nix
+++ default.nix
@@ -1,35 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  buildGradle = pkgs.callPackage ./gradle-env.nix {};
-  unwrappedBuild = buildGradle {
-    envSpec = ./gradle-env.json;
+# Lifted from https://nixos.wiki/wiki/Flakes
 
-    src = pkgs.lib.cleanSourceWith {
-      filter = pkgs.lib.cleanSourceFilter;
-      src = pkgs.lib.cleanSourceWith {
-        filter = path: type: let baseName = baseNameOf path; in !(
-          (type == "directory" && (
-            baseName == "build" ||
-            baseName == ".idea" ||
-            baseName == ".gradle"
-          )) ||
-          (pkgs.lib.hasSuffix ".iml" baseName)
-        );
-        src = ./.;
-      };
-    };
-
-    gradleFlags = [ "installDist" ];
-
-    buildJdk = pkgs.jdk11;
-
-    installPhase = ''
-      mkdir -p  -- "$out"
-      cp -rT -- "build/install/AgoraBot" "$out"
-    '';
- };
-in
-  pkgs.writeShellScriptBin "AgoraBot" ''
-    export JAVA_HOME=${pkgs.lib.escapeShellArg "${pkgs.jdk.home}"}
-    exec ${pkgs.lib.escapeShellArg "${unwrappedBuild}/bin/AgoraBot"} "$@"
-  ''
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  }
+) {
+  src =  ./.;
+}).defaultNix
