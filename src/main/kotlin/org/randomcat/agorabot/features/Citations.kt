09657f9cce1fe9bc4436510b7e79585b6cf53cf8
Jason Cobb <jason.e.cobb@gmail.com>
2022-06-22 20:32:18 -0400

    Add support for separate full and short rule citations

--- src/main/kotlin/org/randomcat/agorabot/features/Citations.kt
+++ src/main/kotlin/org/randomcat/agorabot/features/Citations.kt
@@ -4,21 +4,26 @@ import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.SubscribeEvent
 import org.randomcat.agorabot.*
-import org.randomcat.agorabot.config.parsing.features.CitationsConfig
-import org.randomcat.agorabot.config.parsing.features.readCitationsConfig
 import org.randomcat.agorabot.listener.Command
 import org.randomcat.agorabot.setup.features.featureConfigDir
 import org.randomcat.agorabot.util.await
 import org.randomcat.agorabot.util.coroutineScope
 import org.slf4j.LoggerFactory
 import java.net.URI
+import kotlin.io.path.readText
 
-const val RULE_URL_NUMBER_REPLACMENT = "{bot_rule_num}"
-const val CFJ_URL_NUMBER_REPLACEMENT = "{bot_case_num}"
+private val FULL_RULE_PREFIXES = listOf("R", "FR", "FLR")
+private val SHORT_RULE_PREFIXES = listOf("SR", "SLR")
+private const val RULE_URL_NUMBER_REPLACEMENT = "{bot_rule_num}"
+private const val CFJ_URL_NUMBER_REPLACEMENT = "{bot_case_num}"
 
 private fun String.bracedSections(): List<String> {
     return withIndex()
@@ -45,6 +50,44 @@ private suspend fun sendResponse(message: Message, name: String, citationUri: UR
 
 private val logger = LoggerFactory.getLogger("AgoraBotCitations")
 
+data class CitationsConfig(
+    val fullRuleUrlPattern: String?,
+    val shortRuleUrlPattern: String?,
+    val cfjUrlPattern: String?,
+)
+
+@Serializable
+private data class CitationsConfigDto(
+    @SerialName("rule_url_format")
+    val fullRuleUrlPattern: String? = null,
+    @SerialName("short_rule_url_format")
+    val shortRuleUrlPattern: String? = null,
+    @SerialName("cfj_url_format")
+    val cfjUrlPattern: String? = null,
+) {
+    companion object {
+        private fun checkPatternReplacement(description: String, pattern: String?, replacement: String) {
+            if (pattern == null) return
+
+            require(pattern.contains(replacement)) {
+                "Citation config $description pattern should contain \"$replacement\""
+            }
+        }
+    }
+
+    init {
+        checkPatternReplacement("full rule", fullRuleUrlPattern, RULE_URL_NUMBER_REPLACEMENT)
+        checkPatternReplacement("short rule", shortRuleUrlPattern, RULE_URL_NUMBER_REPLACEMENT)
+        checkPatternReplacement("CFJ", cfjUrlPattern, CFJ_URL_NUMBER_REPLACEMENT)
+    }
+
+    fun toConfig() = CitationsConfig(
+        fullRuleUrlPattern = fullRuleUrlPattern,
+        shortRuleUrlPattern = shortRuleUrlPattern,
+        cfjUrlPattern = cfjUrlPattern,
+    )
+}
+
 private fun citationsFeature(config: CitationsConfig): Feature {
     return object : AbstractFeature() {
         override fun commandsInContext(context: FeatureContext): Map<String, Command> {
@@ -52,7 +95,13 @@ private fun citationsFeature(config: CitationsConfig): Feature {
         }
 
         override fun jdaListeners(context: FeatureContext): List<Any> {
-            if (config.ruleUrlPattern == null && config.cfjUrlPattern == null) return emptyList()
+            if (
+                config.shortRuleUrlPattern == null &&
+                config.fullRuleUrlPattern == null &&
+                config.cfjUrlPattern == null
+            ) {
+                return emptyList()
+            }
 
             return listOf(object {
                 @SubscribeEvent
@@ -92,13 +141,26 @@ private fun citationsFeature(config: CitationsConfig): Feature {
                     }
 
                     for (bracedSection in bracedSections) {
-                        if (config.ruleUrlPattern != null) {
-                            handleParsedNumber(
-                                bracedText = bracedSection,
-                                prefix = "R",
-                                urlPattern = config.ruleUrlPattern,
-                                replacement = RULE_URL_NUMBER_REPLACMENT,
-                            )
+                        if (config.fullRuleUrlPattern != null) {
+                            for (prefix in FULL_RULE_PREFIXES) {
+                                handleParsedNumber(
+                                    bracedText = bracedSection,
+                                    prefix = prefix,
+                                    urlPattern = config.fullRuleUrlPattern,
+                                    replacement = RULE_URL_NUMBER_REPLACEMENT,
+                                )
+                            }
+                        }
+
+                        if (config.shortRuleUrlPattern != null) {
+                            for (prefix in SHORT_RULE_PREFIXES) {
+                                handleParsedNumber(
+                                    bracedText = bracedSection,
+                                    prefix = prefix,
+                                    urlPattern = config.shortRuleUrlPattern,
+                                    replacement = RULE_URL_NUMBER_REPLACEMENT,
+                                )
+                            }
                         }
 
                         if (config.cfjUrlPattern != null) {
@@ -122,7 +184,7 @@ fun citationsFactory() = object : FeatureSource {
         get() = "citations"
 
     override fun readConfig(context: FeatureSetupContext): CitationsConfig {
-        return readCitationsConfig(context.paths.featureConfigDir.resolve("citations.json"))
+        return Json.decodeFromString<CitationsConfigDto>(context.paths.featureConfigDir.resolve("citations.json").readText()).toConfig()
     }
 
     override fun createFeature(config: Any?): Feature {
