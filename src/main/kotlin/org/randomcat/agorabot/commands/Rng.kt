8362ed9726ec2c4c130bfccefdb679df249dfe5c
Jason Cobb <jason.e.cobb@gmail.com>
2022-10-17 22:30:54 -0400

    Support arbitrarily large RNG

--- src/main/kotlin/org/randomcat/agorabot/commands/Rng.kt
+++ src/main/kotlin/org/randomcat/agorabot/commands/Rng.kt
@@ -3,7 +3,7 @@ package org.randomcat.agorabot.commands
 import org.randomcat.agorabot.commands.base.*
 import java.math.BigInteger
 import kotlin.random.Random
-import kotlin.random.nextLong
+import kotlin.random.asJavaRandom
 
 private fun BigInteger.toLongOrNull(): Long? {
     return if (this > Long.MAX_VALUE.toBigInteger() || this < Long.MIN_VALUE.toBigInteger())
@@ -15,39 +15,29 @@ private fun BigInteger.toLongOrNull(): Long? {
 class RngCommand(strategy: BaseCommandStrategy) : BaseCommand(strategy) {
     override fun BaseCommandImplReceiver.impl() {
         matchFirst {
-            args(IntArg("max")) cmd@{ (bigMax) ->
-                val max = bigMax.toLongOrNull() ?: run {
-                    respond("Provided maximum is invalid.")
-                    return@cmd
-                }
-
-                doResponse(min = 1, max = max)
+            args(IntArg("max")) cmd@{ (max) ->
+                doResponse(min = BigInteger.ONE, max = max)
             }
 
-            args(IntArg("min"), IntArg("max")) cmd@{ (bigMin, bigMax) ->
-                val min = bigMin.toLongOrNull() ?: run {
-                    respond("Provided minimum is invalid.")
-                    return@cmd
-                }
-
-                val max = bigMax.toLongOrNull() ?: run {
-                    respond("Provided maximum is invalid.")
-                    return@cmd
-                }
-
+            args(IntArg("min"), IntArg("max")) cmd@{ (min, max) ->
                 doResponse(min = min, max = max)
             }
         }
     }
 
-    private suspend fun BaseCommandExecutionReceiver.doResponse(min: Long, max: Long) {
+    private suspend fun BaseCommandExecutionReceiver.doResponse(min: BigInteger, max: BigInteger) {
         if (min > max) {
             respond("Max value must be at least as big as min value.")
             return
         }
 
-        val result = Random.nextLong(min..max)
+        val length = (max - min) + BigInteger.ONE
+        var offset: BigInteger
 
-        respond("Random number from $min to $max: $result")
+        do {
+            offset = BigInteger(length.bitLength(), Random.asJavaRandom())
+        } while (offset >= length)
+
+        respond("Random number from $min to $max: ${min + offset}")
     }
 }
