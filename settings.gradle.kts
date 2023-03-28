8a301e59f58d07d4d7028ee7325706c874213912
Janet Cobb <jason.e.cobb@gmail.com>
2023-03-28 17:51:13 -0400

    Update gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,9 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
+
 rootProject.name = "AgoraBot"
 
-enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 include(":util")
