b2a6e64b81d8678cd5c99e6355f68b757725e427
Janet Cobb <git@randomcat.org>
2024-03-13 21:32:33 -0400

    Use -opt-in instead of -Xopt-in for Kotlin compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ allprojects {
     }
 
     tasks.withType<KotlinCompile> {
-        kotlinOptions.freeCompilerArgs += listOf("-Xopt-in=kotlin.RequiresOptIn")
+        kotlinOptions.freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn")
     }
 
     tasks.withType<AbstractArchiveTask>().configureEach {
